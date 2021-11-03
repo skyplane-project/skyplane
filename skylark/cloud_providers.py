6fdb01224cf470e97b0a9e726383bbb8e32e5098
Paras Jain <parasj96@gmail.com>
2021-11-03 11:46:28 -0700

    Move AWS provisioning test code to a new testcase

--- skylark/cloud_providers.py
+++ skylark/cloud_providers.py
@@ -142,18 +142,3 @@ class AWSCloudProvider(CloudProvider):
         )
         return AWSServer(f"aws:{region}", instance[0].id)
 
-
-if __name__ == "__main__":
-    aws = AWSCloudProvider()
-
-    grouped_by_region = {}
-    for region in aws.region_list:
-        grouped_by_region[region] = aws.get_matching_instances(region=region)
-        if len(grouped_by_region[region]) == 0:
-            logger.info(f"No instances found in {region}, provisioning")
-            ami = aws.get_ubuntu_ami_id(region)
-            server = aws.provision_instance(region, ami, "i3en.large", f"skylark-{region}")
-            logger.debug(f"Provisioned {server}")
-            grouped_by_region[region].append(server)
-        else:
-            logger.info(f"Found {len(grouped_by_region[region])} instances in {region}")
