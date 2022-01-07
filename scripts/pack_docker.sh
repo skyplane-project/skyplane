684f8d626a8e2161a4e0f8e30ec884547b5c2c6e
Paras Jain <parasj96@gmail.com>
2022-01-06 20:59:49 -0800

    Some minor fixes to build process (#31)

--- scripts/pack_docker.sh
+++ scripts/pack_docker.sh
@@ -10,7 +10,7 @@ cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
 mkdir -p ./dist
 
 echo -e "${BGreen}Building docker image${NC}"
-DOCKER_BUILDKIT=1 sudo docker build -t skylark .
+sudo DOCKER_BUILDKIT=1 docker build -t skylark .
 # sudo docker save skylark | pv > ./dist/skylark.tar
 # du -sh ./dist/skylark.tar
 
