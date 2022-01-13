8cb381d4b32e4e87ca077e4b244a4990feeae08f
Paras Jain <parasj96@gmail.com>
2022-01-13 18:14:17 +0000

    update

--- scripts/experiments/num_gateways/benchmark_num_gateways.sh
+++ scripts/experiments/num_gateways/benchmark_num_gateways.sh
@@ -4,7 +4,7 @@ SRC_REGION=$1
 DST_REGION=$2
 INTER_REGION=$3
 
-NUM_GATEWAYS=${4:-1}
+NUM_CONNECTIONS=${4:-64}
 CHUNK_SIZE_MB=${5:-16}
 N_CHUNKS_PER_CONNECTION=${6:-512}
 
@@ -18,9 +18,10 @@ fi
 function benchmark_config {
     NUM_CONNECTIONS=$1
     NUM_GATEWAYS=$2
-    # N_CHUNKS_PER_CONNECTION * NUM_CONNECTIONS
-    N_CHUNKS=$((N_CHUNKS_PER_CONNECTION * NUM_CONNECTIONS))
-    echo "skylark replicate-random $SRC_REGION $DST_REGION $INTER_REGION --chunk-size-mb $CHUNK_SIZE_MB --n-chunks $N_CHUNKS --num-gateways $NUM_GATEWAYS --num-outgoing-connections $NUM_CONNECTIONS --no-reuse-gateways --no-serve-web-dashboard"
+    N_CHUNKS=$((N_CHUNKS_PER_CONNECTION * NUM_CONNECTIONS * NUM_GATEWAYS))
+    set -x
+    skylark replicate-random $SRC_REGION $DST_REGION $INTER_REGION --chunk-size-mb $CHUNK_SIZE_MB --n-chunks $N_CHUNKS --num-gateways $NUM_GATEWAYS --num-outgoing-connections $NUM_CONNECTIONS --no-serve-web-dashboard
+    set +x
 }
 
 # log function with message argument
@@ -31,7 +32,7 @@ function log() {
 }
 
 EXP_ID="$SRC_REGION-$DST_REGION-$(./scripts/utils/get_random_word_hash.sh)"
-LOG_DIR=data/experiments/benchmark_num_connections/logs/$EXP_ID
+LOG_DIR=data/experiments/benchmark_num_gateways/logs/$EXP_ID
 log "Creating log directory $LOG_DIR"
 log "Experiment ID: $EXP_ID"
 rm -rf $LOG_DIR
@@ -48,26 +49,16 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-
 # assert gnu parallel is installed
 if ! [ -x "$(command -v parallel)" ]; then
     log "Error: gnu parallel is not installed"
     exit 1
 fi
 
-# make list of commands to run with gnu parallel (one for each inter-region) and save to $PARALLEL_CMD_LIST (one command per line)
-PARALLEL_CMD_LIST=""
-# powers of 2
-for NUM_CONNECTIONS in 1 2 4 8 16 32 64 128 144; do
-    PARALLEL_CMD_LIST="$PARALLEL_CMD_LIST\n$(benchmark_config $NUM_CONNECTIONS $NUM_GATEWAYS) &> $LOG_DIR/$NUM_CONNECTIONS.log"
+for NUM_GATEWAYS in 1 2 4; do
+    log "Running benchmark with $NUM_GATEWAYS gateways"
+    benchmark_config $NUM_CONNECTIONS $NUM_GATEWAYS &> $LOG_DIR/$NUM_GATEWAYS.log
 done
-log "Running commands with gnu parallel:"
-echo -e "$PARALLEL_CMD_LIST\n"
-echo -e "$PARALLEL_CMD_LIST\n" >> $LOG_DIR/launch.log
-
-log "Parallel:"
-parallel -j 8 --results $LOG_DIR/raw_logs --joblog $LOG_DIR/parallel_joblog.txt --eta < <(echo -e "$PARALLEL_CMD_LIST")
-
 
 log "Stopping instances"
 skylark deprovision &>> $LOG_DIR/launch.log
