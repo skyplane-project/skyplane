63fa2365f9b270b3cd0231c7920251736914e7a0
Sarah Wooders <sarahwooders@gmail.com>
2022-01-22 07:05:49 +0000

    add test script for gcs

--- skylark/obj_store/gcs_interface.py
+++ skylark/obj_store/gcs_interface.py
@@ -79,60 +79,54 @@ class GCSInterface(ObjectStoreInterface):
     #        batch, keys = keys[:1000], keys[1000:]  # take up to 1000 keys at a time
     #        s3_client.delete_objects(Bucket=self.bucket_name, Delete={"Objects": [{"Key": k} for k in batch]})
 
-    #def get_obj_metadata(self, obj_name):
-    #    s3_resource = AWSServer.get_boto3_resource("s3", self.aws_region).Bucket(self.bucket_name)
-    #    try:
-    #        return s3_resource.Object(str(obj_name).lstrip("/"))
-    #    except botocore.exceptions.ClientError as e:
-    #        raise NoSuchObjectException(f"Object {obj_name} does not exist, or you do not have permission to access it") from e
+    def get_obj_metadata(self, obj_name):
+        bucket = self._gcs_client.bucket(self.bucket_name)
+        blob = bucket.get_blob(obj_name)
+        if blob is None:
+            raise NoSuchObjectException(f"Object {obj_name} does not exist, or you do not have permission to access it") 
+        return blob
 
-    #def get_obj_size(self, obj_name):
-    #    return self.get_obj_metadata(obj_name).content_length
+    def get_obj_size(self, obj_name):
+        return self.get_obj_metadata(obj_name).size
 
-    #def exists(self, obj_name):
-    #    try:
-    #        self.get_obj_metadata(obj_name)
-    #        return True
-    #    except NoSuchObjectException:
-    #        return False
+    def exists(self, obj_name):
+        try:
+            self.get_obj_metadata(obj_name)
+            return True
+        except NoSuchObjectException:
+            return False
 
-    ## todo: implement range request for download
-    #def download_object(self, src_object_name, dst_file_path) -> Future:
-    #    src_object_name, dst_file_path = str(src_object_name), str(dst_file_path)
-    #    src_object_name = "/" + src_object_name if src_object_name[0] != "/" else src_object_name
-    #    download_headers = HttpHeaders([("host", self.bucket_name + ".s3." + self.aws_region + ".amazonaws.com")])
-    #    request = HttpRequest("GET", src_object_name, download_headers)
+    # todo: implement range request for download
+    def download_object(self, src_object_name, dst_file_path) -> Future:
+        src_object_name, dst_file_path = str(src_object_name), str(dst_file_path)
+        src_object_name = src_object_name if src_object_name[0] != "/" else src_object_name
 
-    #    def _on_body_download(offset, chunk, **kwargs):
-    #        if not os.path.exists(dst_file_path):
-    #            open(dst_file_path, "a").close()
-    #        with open(dst_file_path, "rb+") as f:
-    #            f.seek(offset)
-    #            f.write(chunk)
+        bucket = self._gcs_client.bucket(self.bucket_name)
+        blob = bucket.blob(src_object_name)
+        chunk = blob.download_as_string()
 
-    #    return self._s3_client.make_request(
-    #        recv_filepath=dst_file_path,
-    #        request=request,
-    #        type=S3RequestType.GET_OBJECT,
-    #        on_done=self._on_done_download,
-    #        on_body=_on_body_download,
-    #    ).finished_future
+        def _on_body_download(offset, chunk, **kwargs):
+            if not os.path.exists(dst_file_path):
+                open(dst_file_path, "a").close()
+            with open(dst_file_path, "rb+") as f:
+                f.seek(offset)
+                f.write(chunk)
 
-    #def upload_object(self, src_file_path, dst_object_name, content_type="infer") -> Future:
-    #    print("uploading object", src_file_path, dst_object_name)
-    #    src_file_path, dst_object_name = str(src_file_path), str(dst_object_name)
-    #    dst_object_name = "/" + dst_object_name if dst_object_name[0] != "/" else dst_object_name
-    #    content_len = os.path.getsize(src_file_path)
-    #    if content_type == "infer":
-    #        content_type = mimetypes.guess_type(src_file_path)[0] or "application/octet-stream"
-    #    upload_headers = HttpHeaders()
-    #    upload_headers.add("host", self.bucket_name + ".s3." + self.aws_region + ".amazonaws.com")
-    #    upload_headers.add("Content-Type", content_type)
-    #    upload_headers.add("Content-Length", str(content_len))
-    #    request = HttpRequest("PUT", dst_object_name, upload_headers)
-    #    return self._s3_client.make_request(
-    #        send_filepath=src_file_path,
-    #        request=request,
-    #        type=S3RequestType.PUT_OBJECT,
-    #        on_done=self._on_done_upload,
-    #    ).finished_future
+        # TODO: create future?
+        _on_body_download(0, chunk)
+
+
+    def upload_object(self, src_file_path, dst_object_name, content_type="infer") -> Future:
+        print("uploading object", src_file_path, dst_object_name)
+        src_file_path, dst_object_name = str(src_file_path), str(dst_object_name)
+        dst_object_name = dst_object_name if dst_object_name[0] != "/" else dst_object_name
+        content_len = os.path.getsize(src_file_path)
+
+        if content_type == "infer":
+            content_type = mimetypes.guess_type(src_file_path)[0] or "application/octet-stream"
+
+        bucket = self._gcs_client.bucket(self.bucket_name)
+        blob = bucket.blob(dst_object_name)
+
+        # TODO: create future? 
+        blob.upload_from_filename(src_file_path)
---
+++ skylark/test/test_gcs_interface.py
@@ -0,0 +1,44 @@
+import hashlib
+import os
+import tempfile
+from skylark import MB
+
+from skylark.obj_store.gcs_interface import GCSInterface
+from skylark.utils.utils import Timer
+
+
+def test_gcs_interface():
+    gcs_interface = GCSInterface(f"us-east1", f"sky-us-east-1", True)
+    assert gcs_interface.bucket_name == "sky-us-east-1"
+    assert gcs_interface.gcp_region == "us-east1"
+    gcs_interface.create_bucket()
+
+    # generate file and upload
+    obj_name = "test.txt"
+    file_size_mb = 128
+    with tempfile.NamedTemporaryFile() as tmp:
+        fpath = tmp.name
+        with open(fpath, "wb") as f:
+            f.write(os.urandom(int(file_size_mb * MB)))
+        file_md5 = hashlib.md5(open(fpath, "rb").read()).hexdigest()
+
+        with Timer() as t:
+            upload_future = gcs_interface.upload_object(fpath, obj_name)
+            #upload_future.result() # TODO: make future
+        assert gcs_interface.get_obj_size(obj_name) == os.path.getsize(fpath)
+        assert gcs_interface.exists(obj_name)
+        assert not gcs_interface.exists("random_nonexistent_file")
+
+    # download object
+    with tempfile.NamedTemporaryFile() as tmp:
+        fpath = tmp.name
+        if os.path.exists(fpath):
+            os.remove(fpath)
+        with Timer() as t:
+            download_future = gcs_interface.download_object(obj_name, fpath)
+            #download_future.result() # TODO: make future
+
+        # check md5
+        dl_file_md5 = hashlib.md5(open(fpath, "rb").read()).hexdigest()
+        assert dl_file_md5 == file_md5
+
