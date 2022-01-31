ce332470eaf4e427d548d1b8fb95d393b4b58aa8
Paras Jain <parasj96@gmail.com>
2022-01-31 11:47:51 -0800

    Gurobi works

--- skylark/replicate/solver.py
+++ skylark/replicate/solver.py
@@ -56,15 +56,15 @@ class ThroughputSolution:
 
 class ThroughputSolver:
     def __init__(self, df_path, default_throughput=0.0):
-        self.df = pd.read_csv(df_path).set_index(["src_region", "dst_region"])
+        self.df = pd.read_csv(df_path).set_index(["src_region", "dst_region", "src_tier", "dst_tier"]).sort_index()
         self.default_throughput = default_throughput
 
-    def get_path_throughput(self, src, dst):
+    def get_path_throughput(self, src, dst, src_tier="PREMIUM", dst_tier="PREMIUM"):
         if src == dst:
             return self.default_throughput
-        elif (src, dst) not in self.df.index:
+        elif (src, dst, src_tier, dst_tier) not in self.df.index:
             return None
-        return self.df.loc[(src, dst), "throughput_sent"]
+        return self.df.loc[(src, dst, src_tier, dst_tier), "throughput_sent"].values[0]
 
     def get_path_cost(self, src, dst):
         return CloudProvider.get_transfer_cost(src, dst)
@@ -126,8 +126,6 @@ class ThroughputSolverILP(ThroughputSolver):
     def solve_min_cost(
         self, p: ThroughputProblem, instance_cost_multipler: float = 1.0, solver=cp.GLPK, solver_verbose=False, save_lp_path=None
     ):
-        logger.debug(f"Solving for problem {p}")
-
         regions = self.get_regions()
         sources = [regions.index(p.src)]
         sinks = [regions.index(p.dst)]
