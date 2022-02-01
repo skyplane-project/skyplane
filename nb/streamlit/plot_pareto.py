dde518dc954d83dd8bfd3765381dadbc753512b8
Paras Jain <parasj96@gmail.com>
2022-02-01 07:42:00 +0000

    Full solves done

--- nb/streamlit/plot_pareto.py
+++ nb/streamlit/plot_pareto.py
@@ -1,3 +1,4 @@
+import subprocess
 import os
 from pathlib import Path
 import pickle
@@ -17,26 +18,29 @@ plt.style.use("seaborn-bright")
 plt.set_cmap("plasma")
 
 out_fname = data_dir / "pareto_data.df.parquet"
-source_bucket_path = st.sidebar.text_input("S3 bucket prefix", "s3://skylark-optimizer-results/pareto_data")
-source_bucket_experiment_tag = st.sidebar.text_input("Experiment tag", "2022.01.31_12.34_63c39402-9e7f-4d36-a850-c5151890ff39")
-st.sidebar.button("Recompute dataframe", on_click=lambda: Path(out_fname).unlink(missing_ok=True))
+source_bucket_path = st.text_input("S3 bucket prefix", "s3://skylark-optimizer-results/pareto_data")
+source_bucket_experiment_tag = st.text_input("Experiment tag", "2022.02.01_00.07_e86e33b2-bccf-4509-80e4-1f2ac62d7c54")
+st.button("Recompute dataframe", on_click=lambda: Path(out_fname).unlink(missing_ok=True))
 
 def download_and_parse_data(out_fname):
     out_dir = data_dir / 'pareto_raw_data' / source_bucket_experiment_tag
     if not out_fname.exists():
         st.info("Parsing data, this will take some time...")
         s3_path = os.path.join(source_bucket_path, source_bucket_experiment_tag)
-        os.system(f"aws s3 sync {s3_path} {out_dir}")
-
+        st.info(f"aws s3 sync {s3_path} {out_dir}")
+        with st.spinner("Downloading data..."):
+            subprocess.run(f"aws s3 sync {s3_path} {out_dir}", shell=True)
+        
         rows = []
         for file in out_dir.glob("*.pkl"):
-            with open(file, "rb") as f:
-                for i in pickle.load(f):
-                    x = i.__dict__.copy()
-                    for k, v in x['problem'].__dict__.items():
-                        x[f"problem_{k}"] = v
-                    del x['problem']
-                    rows.append(x)
+            with st.spinner(f"Parsing {file.name}..."):
+                with open(file, "rb") as f:
+                    for i in pickle.load(f):
+                        x = i.__dict__.copy()
+                        for k, v in x['problem'].__dict__.items():
+                            x[f"problem_{k}"] = v
+                        del x['problem']
+                        rows.append(x)
         df = pd.DataFrame(rows)
         st.info(f"Saving data to {out_fname}, has {len(df)} rows")
         df = df.drop(columns=["var_edge_flow_gigabits", "var_conn", "var_instances_per_region", "cost_egress_by_edge"])
@@ -60,22 +64,60 @@ src_regions_choices = sorted(df["problem_src"].unique())
 dst_regions_choices = sorted(df["problem_dst"].unique())
 instance_choices = sorted(df["problem_instance_limit"].unique())
 
-st.sidebar.subheader("Filter by source region")
-st.sidebar.write(f"Sources: {', '.join(src_regions_choices)}")
-src_prefix = st.sidebar.text_input("Source region prefix", src_regions_choices[0])
-st.sidebar.subheader("Filter by destination region")
-dst_prefix = st.sidebar.text_input("Destination region prefix", "")
-st.sidebar.subheader("Filter by instance limit")
-instance_limit = st.sidebar.selectbox("Instance limit", instance_choices)
+########
+# Plot geomean
+########
+
+# for each src, dest pair, compute the geometric mean of throughput speedup
+# def calc_geomean(x):
+#     if len(x) == 0:
+#         return np.nan
+#     return np.exp(np.mean(np.log(x)))
+
+# st.write("Geomean speedup")
+# fig, ax = plt.subplots(figsize=(8, 6))
+# for (src_region, dst_region), df_grouped in df.groupby(["problem_src", "problem_dst"]):
+#     df_grouped["throughput_speedup"] = df_grouped["throughput_achieved_gbits"] / df_grouped["baseline_throughput_achieved_gbits"]
+#     df_grouped["cost_increase"] = df_grouped["cost_total"] / min(df_grouped["cost_total"])
+#     df_grouped = df_grouped[df_grouped["throughput_speedup"] > 1]
+#     df_grouped.sort_values(by="throughput_speedup", inplace=True)
+
+#     x = df_grouped["throughput_speedup"]
+#     y = df_grouped["cost_increase"]
+#     # add (1, 1)
+#     x = [1] + list(x)
+#     y = [1] + list(y)
+#     label = "{} to {}".format(src_region, dst_region)
+#     ax.plot(x, y, label=label, alpha=0.8)
+# ax.set_xlabel("Throughput speedup (x)")
+# ax.set_ylabel("Cost increase (x)")
+# fig.set_facecolor("white")
+# st.pyplot(fig, bbox_inches="tight")
+
+########
+# Filter results to a single src, dest pair
+########
+
+st.subheader("Filter by source region")
+st.write(f"Sources: {', '.join(src_regions_choices)}")
+src_prefix = st.text_input("Source region prefix", "")
+st.subheader("Filter by destination region")
+dst_prefix = st.text_input("Destination region prefix", "")
+st.subheader("Filter by instance limit")
+instance_limit = st.selectbox("Instance limit", instance_choices)
 
 df = df[df["problem_src"].str.startswith(src_prefix)]
 df = df[df["problem_dst"].str.startswith(dst_prefix)]
 df = df[df["problem_instance_limit"] == instance_limit]
 st.info(f"Filtered to {len(df)} rows")
 
-if len(df) > 1000:
-    st.warning("Too many rows to plot. Only plotting the first 1000.")
-    df = df.iloc[:1000]
+if len(df) > 10000:
+    st.warning("Too many rows to plot. Only plotting the first 10000.")
+    df = df.iloc[:10000]
+
+########
+# Plot geomean
+########
 
 st.subheader("Throughput versus cost")
 fig, ax = plt.subplots(figsize=(8, 6))
@@ -89,28 +131,33 @@ for (src_region, dst_region), df_grouped in df.groupby(["problem_src", "problem_
     x = [baseline_throughput] + x.tolist()
     y = [baseline_cost] + y.tolist()
     ax.plot(x, y, label=label, marker="o", linestyle="--")
-ax.legend(loc="lower center", bbox_to_anchor=(0.5, -0.6), ncol=3)
+if st.checkbox("Show legend (absolute)", value=True):
+    ax.legend(loc="lower center", bbox_to_anchor=(0.5, -0.6), ncol=3)
 ax.set_xlabel("Throughput (Gbps)")
 ax.set_ylabel("Cost ($/GB)")
 fig.set_facecolor("white")
 st.pyplot(fig, bbox_inches="tight")
 
-# st.write("Throughput speedup versus cost increase")
-# fig, ax = plt.subplots(figsize=(8, 6))
-# for (src_region, dst_region), df_grouped in df.groupby(["problem_src", "problem_dst"]):
-#     df_grouped["throughput_speedup"] = df_grouped["throughput_achieved_gbits"] / df_grouped["baseline_throughput_gbits"]
-#     df_grouped["cost_increase"] = df_grouped["cost_total"] / min(df_grouped["cost_total"])
-#     df_grouped.sort_values(by="throughput_speedup", inplace=True)
+st.write("Throughput speedup versus cost increase")
+fig, ax = plt.subplots(figsize=(8, 6))
+for (src_region, dst_region), df_grouped in df.groupby(["problem_src", "problem_dst"]):
+    df_grouped["throughput_speedup"] = df_grouped["throughput_achieved_gbits"] / df_grouped["baseline_throughput_achieved_gbits"]
+    df_grouped["cost_increase"] = df_grouped["cost_total"] / min(df_grouped["cost_total"])
+    df_grouped = df_grouped[df_grouped["throughput_speedup"] > 1]
+    df_grouped.sort_values(by="throughput_speedup", inplace=True)
 
-#     x = df_grouped["throughput_speedup"]
-#     y = df_grouped["cost_increase"]
-#     # add (1, 1)
-#     x = [1] + list(x)
-#     y = [1] + list(y)
-#     label = "{} to {}".format(src_region, dst_region)
-#     ax.plot(x, y, label=label, alpha=0.8)
-# ax.legend(loc="lower center", bbox_to_anchor=(0.5, -0.6), ncol=3)
-# ax.set_xlabel("Throughput speedup (x)")
-# ax.set_ylabel("Cost increase (x)")
-# fig.set_facecolor("white")
-# st.pyplot(fig, bbox_inches="tight")
+    x = df_grouped["throughput_speedup"]
+    y = df_grouped["cost_increase"]
+    # add (1, 1)
+    x = [1] + list(x)
+    y = [1] + list(y)
+    label = "{} to {}".format(src_region, dst_region)
+    ax.plot(x, y, label=label, alpha=0.8)
+if st.checkbox("Show legend (speedup)", value=True):
+    ax.legend(loc="lower center", bbox_to_anchor=(0.5, -0.6), ncol=3)
+ax.set_xlabel("Throughput speedup (x)")
+ax.set_ylabel("Cost increase (x)")
+fig.set_facecolor("white")
+st.pyplot(fig, bbox_inches="tight")
+
+
