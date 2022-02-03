47879d2cc7a1c00b05f94b09e54cb2ae46798f6b
Paras Jain <parasj96@gmail.com>
2022-02-03 14:44:19 -0800

    Updated plotting (#134)

---
+++ nb/streamlit/common.py
@@ -0,0 +1,71 @@
+import gc
+import subprocess
+import os
+from pathlib import Path
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+from skylark import skylark_root
+
+data_dir = skylark_root / "data"
+
+@st.cache(suppress_st_warning=True)
+def load_data_impl(source_bucket_path, source_bucket_experiment_tag, out_dir, out_fname):
+    if not out_fname.exists():
+        st.info("Parsing data, this will take some time...")
+        s3_path = os.path.join(source_bucket_path, source_bucket_experiment_tag)
+        st.info(f"aws s3 sync {s3_path} {out_dir}")
+        with st.spinner("Downloading data..."):
+            subprocess.run(f"aws s3 sync {s3_path} {out_dir}", shell=True)
+        
+        dfs = []
+        for file in out_dir.glob("*.pkl"):
+            with st.spinner(f"Parsing {file.name}..."):
+                rows = []
+                with open(file, "rb") as f:
+                    data_pickle = pickle.load(f)
+                n_rows = len(data_pickle)
+                pbar = st.progress(0)
+                while data_pickle:
+                    i = data_pickle.pop()
+                    x = i.__dict__.copy()
+                    for k, v in x['problem'].__dict__.items():
+                        x[f"problem_{k}"] = v
+                    del x['problem']
+                    del x['problem_const_throughput_grid_gbits']
+                    del x['problem_const_cost_per_gb_grid']
+                    del x['extra_data']
+                    del x['var_edge_flow_gigabits']
+                    del x['var_conn']
+                    del x['var_instances_per_region']
+                    del x['cost_egress_by_edge']
+                    rows.append(x)
+                    pbar.progress(len(rows) / n_rows)
+                del data_pickle
+                dfs.append(pd.DataFrame(rows))
+                st.write(f"Parsed {len(rows)} rows from {file.name}")
+                del rows
+                gc.collect()
+        df = pd.concat(dfs)
+        st.info(f"Saving data to {out_fname}, has {len(df)} rows")
+        df.to_parquet(out_fname)
+    else:
+        df = pd.read_parquet(out_fname)
+
+    df = df[df.is_feasible].dropna()
+    df['throughput_achieved_gbits'] = df['throughput_achieved_gbits'].apply(lambda x: x[0])
+    df["problem_src_region"] = df["problem_src"].apply(lambda x: x.split("-")[0])
+    df["problem_dst_region"] = df["problem_dst"].apply(lambda x: x.split("-")[0])
+    return df
+
+def load_data(default_experiment_tag="2022.02.01_02.25_d20ccd26-93dd-483d-976b-91204e97c417"):
+    st.sidebar.subheader(f"Select experiment")
+    source_bucket_path = st.sidebar.text_input("S3 bucket prefix", "s3://skylark-optimizer-results/pareto_data")
+    source_bucket_experiment_tag = st.sidebar.text_input("Experiment tag", default_experiment_tag)
+    out_dir = data_dir / 'pareto_raw_data' / source_bucket_experiment_tag
+    out_fname = out_dir / "pareto_data.parquet"
+    out_dir.mkdir(exist_ok=True, parents=True)
+    st.sidebar.button("Recompute dataframe", on_click=lambda: Path(out_fname).unlink(missing_ok=True))
+    df = load_data_impl(source_bucket_path, source_bucket_experiment_tag, out_dir, out_fname).copy()
+    return df, out_dir
--- nb/streamlit/plot_pareto.py
+++ nb/streamlit/plot_pareto.py
@@ -2,12 +2,10 @@ import subprocess
 import os
 from pathlib import Path
 import pickle
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from tqdm.notebook import tqdm
-import seaborn as sns
 import streamlit as st
+from nb.streamlit.common import load_data
 from skylark import skylark_root
 
 data_dir = skylark_root / "data"
@@ -17,45 +15,7 @@ figure_dir.mkdir(exist_ok=True, parents=True)
 plt.style.use("seaborn-bright")
 plt.set_cmap("plasma")
 
-out_fname = data_dir / "pareto_data.df.parquet"
-source_bucket_path = st.text_input("S3 bucket prefix", "s3://skylark-optimizer-results/pareto_data")
-source_bucket_experiment_tag = st.text_input("Experiment tag", "2022.02.01_00.07_e86e33b2-bccf-4509-80e4-1f2ac62d7c54")
-st.button("Recompute dataframe", on_click=lambda: Path(out_fname).unlink(missing_ok=True))
-
-def download_and_parse_data(out_fname):
-    out_dir = data_dir / 'pareto_raw_data' / source_bucket_experiment_tag
-    if not out_fname.exists():
-        st.info("Parsing data, this will take some time...")
-        s3_path = os.path.join(source_bucket_path, source_bucket_experiment_tag)
-        st.info(f"aws s3 sync {s3_path} {out_dir}")
-        with st.spinner("Downloading data..."):
-            subprocess.run(f"aws s3 sync {s3_path} {out_dir}", shell=True)
-        
-        rows = []
-        for file in out_dir.glob("*.pkl"):
-            with st.spinner(f"Parsing {file.name}..."):
-                with open(file, "rb") as f:
-                    for i in pickle.load(f):
-                        x = i.__dict__.copy()
-                        for k, v in x['problem'].__dict__.items():
-                            x[f"problem_{k}"] = v
-                        del x['problem']
-                        rows.append(x)
-        df = pd.DataFrame(rows)
-        st.info(f"Saving data to {out_fname}, has {len(df)} rows")
-        df = df.drop(columns=["var_edge_flow_gigabits", "var_conn", "var_instances_per_region", "cost_egress_by_edge"])
-        df.to_parquet(out_fname)
-    return pd.read_parquet(out_fname)
-
-def parse_data(df):
-    df = df.drop(columns=["problem_const_throughput_grid_gbits", "problem_const_cost_per_gb_grid", "extra_data"])
-    df = df[df.is_feasible].dropna()
-    df['throughput_achieved_gbits'] = df['throughput_achieved_gbits'].apply(lambda x: x[0])
-    df["problem_src_region"] = df["problem_src"].apply(lambda x: x.split("-")[0])
-    df["problem_dst_region"] = df["problem_dst"].apply(lambda x: x.split("-")[0])
-    return df
-
-df = parse_data(download_and_parse_data(out_fname))
+df, out_dir = load_data()
 
 # select rows where throughput_achieved_gbits >= baseline_throughput_gbits
 # df = df[df["throughput_achieved_gbits"] >= df["baseline_achieved_throughput_gbits"]]
@@ -98,18 +58,18 @@ instance_choices = sorted(df["problem_instance_limit"].unique())
 # Filter results to a single src, dest pair
 ########
 
-st.subheader("Filter by source region")
-st.write(f"Sources: {', '.join(src_regions_choices)}")
-src_prefix = st.text_input("Source region prefix", "")
-st.subheader("Filter by destination region")
-dst_prefix = st.text_input("Destination region prefix", "")
-st.subheader("Filter by instance limit")
-instance_limit = st.selectbox("Instance limit", instance_choices)
+st.sidebar.subheader("Filter by source region")
+st.sidebar.write(f"Sources: {', '.join(src_regions_choices)}")
+src_prefix = st.sidebar.text_input("Source region prefix", "azure:")
+st.sidebar.subheader("Filter by destination region")
+dst_prefix = st.sidebar.text_input("Destination region prefix", "azure:")
+st.sidebar.subheader("Filter by instance limit")
+instance_limit = st.sidebar.selectbox("Instance limit", instance_choices)
 
 df = df[df["problem_src"].str.startswith(src_prefix)]
 df = df[df["problem_dst"].str.startswith(dst_prefix)]
 df = df[df["problem_instance_limit"] == instance_limit]
-st.info(f"Filtered to {len(df)} rows")
+st.sidebar.info(f"Filtered to {len(df)} rows")
 
 if len(df) > 10000:
     st.warning("Too many rows to plot. Only plotting the first 10000.")
@@ -118,6 +78,11 @@ if len(df) > 10000:
 ########
 # Plot geomean
 ########
+
+########
+# Plot filtered
+########
+
 
 st.subheader("Throughput versus cost")
 fig, ax = plt.subplots(figsize=(8, 6))
