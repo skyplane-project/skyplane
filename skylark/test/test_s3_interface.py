b5a70eeb922ddc8ffd1353f402a8d35d5b26afa7
milesturin <milesturin@gmail.com>
2022-04-06 12:32:38 -0700

    got s3 interface tests working with the migration

--- skylark/test/test_s3_interface.py
+++ skylark/test/test_s3_interface.py
@@ -8,22 +8,24 @@ from skylark.utils.utils import Timer
 
 
 def test_s3_interface():
-    s3_interface = S3Interface("us-east-1", "skylark-test-us-east-1", True)
+    s3_interface = S3Interface("us-east-1", "sky-us-east-1", True)
     assert s3_interface.aws_region == "us-east-1"
-    assert s3_interface.bucket_name == "skylark-test-us-east-1"
+    assert s3_interface.bucket_name == "sky-us-east-1"
     s3_interface.create_bucket()
 
     # generate file and upload
-    obj_name = "/test.txt"
+    obj_name = "/test_small.txt"
     file_size_mb = 128
     with tempfile.NamedTemporaryFile() as tmp:
         fpath = tmp.name
-        with open(fpath, "wb") as f:
+        with open(fpath, "rb+") as f:
             f.write(os.urandom(int(file_size_mb * MB)))
-        file_md5 = hashlib.md5(open(fpath, "rb").read()).hexdigest()
+            f.seek(0)
+            file_md5 = hashlib.md5(f.read()).hexdigest()
 
         with Timer() as t:
-            s3_interface.upload_object(fpath, obj_name)
+            s3_interface.upload_object(fpath, obj_name).result()
+
         assert s3_interface.get_obj_size(obj_name) == os.path.getsize(fpath)
 
     # download object
@@ -32,9 +34,12 @@ def test_s3_interface():
         if os.path.exists(fpath):
             os.remove(fpath)
         with Timer() as t:
-            s3_interface.download_object(obj_name, fpath)
+            s3_interface.download_object(obj_name, fpath).result()
+
         assert s3_interface.get_obj_size(obj_name) == os.path.getsize(fpath)
 
         # check md5
-        dl_file_md5 = hashlib.md5(open(fpath, "rb").read()).hexdigest()
+        with open(fpath, "rb") as f:
+            dl_file_md5 = hashlib.md5(f.read()).hexdigest()
+
         assert dl_file_md5 == file_md5
