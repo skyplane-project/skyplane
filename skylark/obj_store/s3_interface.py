f69c5eba11cb648433b60af6ec572c6f8dc7a876
milesturin <milesturin@gmail.com>
2022-04-26 22:05:45 -0700

    moved back to managed transfers for non-multipart, removed cannonicalization

--- skylark/obj_store/s3_interface.py
+++ skylark/obj_store/s3_interface.py
@@ -74,28 +74,24 @@ class S3Interface(ObjectStoreInterface):
 
     def download_object(self, src_object_name, dst_file_path, byte_offset=None, byte_count=None):
         src_object_name, dst_file_path = str(src_object_name), str(dst_file_path)
-        src_object_name = "/" + src_object_name if src_object_name[0] != "/" else src_object_name
         s3_client = self.auth.get_boto3_client("s3", self.aws_region)
-        parameters = {
-                "Bucket": self.bucket_name,
-                "Key": src_object_name
-        }
         if byte_offset is None or byte_count is None:
-            #unoptimized
-            byte_offset = 0
+            s3_client.download_file(self.bucket_name, src_object_name, dst_file_path)
         else:
-            parameters["Range"] = f"bytes={byte_offset}-{byte_offset + byte_count - 1}"
-        response = s3_client.get_object(**parameters)
-        if not os.path.exists(dst_file_path):
-            open(dst_file_path, "a").close()
-        with open(dst_file_path, "rb+") as f:
-            f.seek(byte_offset)
-            f.write(response["Body"].read())
-        response["Body"].close() 
+            response = s3_client.get_object(
+                Bucket=self.bucket_name,
+                Key=src_object_name,
+                Range=f"bytes={byte_offset}-{byte_offset + byte_count - 1}"
+            )
+            if not os.path.exists(dst_file_path):
+                open(dst_file_path, "a").close()
+            with open(dst_file_path, "rb+") as f:
+                f.seek(byte_offset)
+                f.write(response["Body"].read())
+            response["Body"].close() 
 
     def initiate_multipart_upload(self, dst_object_name, content_type):
         #cannot infer content type here
-        dst_object_name = "/" + dst_object_name if dst_object_name[0] != "/" else dst_object_name
         s3_client = self.auth.get_boto3_client("s3", self.aws_region)
         response = s3_client.create_multipart_upload(
             Bucket=self.bucket_name,
@@ -107,9 +103,10 @@ class S3Interface(ObjectStoreInterface):
     def upload_object(self, src_file_path, dst_object_name, upload_id=None, byte_offset=None, byte_count=None, part_number=None):
         assert part_number is None or 1 <= part_number <= 10000, f"invalid part_number {part_number}, should be in range [1, 10000]" 
         dst_object_name, src_file_path = str(dst_object_name), str(src_file_path)
-        dst_object_name = "/" + dst_object_name if dst_object_name[0] != "/" else dst_object_name
-        def _upload_object_part_helper():
-            s3_client = self.auth.get_boto3_client("s3", self.aws_region)
+        s3_client = self.auth.get_boto3_client("s3", self.aws_region)
+        if upload_id is None or byte_offset is None or byte_count is None or part_number is None:
+            s3_client.upload_file(src_file_path, self.bucket_name, dst_object_name)
+        else:
             with open(src_file_path, mode="rb+") as f:
                 f.seek(byte_offset)
                 response = s3_client.upload_part(
@@ -121,21 +118,8 @@ class S3Interface(ObjectStoreInterface):
                     ContentLength=byte_count
             )
             return {"ETag": response["ETag"], "PartNumber": part_number} #user should build a list of these
-             
-        if upload_id is None or byte_offset is None or byte_count is None or part_number is None:
-            #upload entire object
-            inferred_type = mimetypes.guess_type(src_file_path)[0] or "application/octet-stream"
-            upload_id = self.initiate_multipart_upload(dst_object_name, inferred_type)
-            byte_offset = 0
-            byte_count = os.path.getsize(src_file_path)
-            part_number = 1
-            part_list = [_upload_object_part_helper()]
-            return self.finalize_multipart_upload(dst_object_name, upload_id, part_list)
-        else:
-            return _upload_object_part_helper()
 
     def finalize_multipart_upload(self, dst_object_name, upload_id, part_list):
-        dst_object_name = "/" + dst_object_name if dst_object_name[0] != "/" else dst_object_name
         part_list.sort(key=lambda d: d["PartNumber"]) #list sorting is handled here, not left to user
         s3_client = self.auth.get_boto3_client("s3", self.aws_region)
         response = s3_client.complete_multipart_upload(
@@ -144,4 +128,3 @@ class S3Interface(ObjectStoreInterface):
                 Key=dst_object_name,
                 MultipartUpload={"Parts": part_list}
         )
-        return response["ETag"]
