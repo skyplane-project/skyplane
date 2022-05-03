82e7fb74b39a7b47ac5c9349d3e0abdde6cd9ed7
Miles Turin <31150941+milesturin@users.noreply.github.com>
2022-05-02 17:56:27 -0700

    S3 Multipart Uploads & Downlods (#270)

--- skylark/obj_store/s3_interface.py
+++ skylark/obj_store/s3_interface.py
@@ -1,17 +1,14 @@
 from typing import Iterator, List
-
+import os
 import botocore.exceptions
-from boto3.s3.transfer import TransferConfig
 from skylark import exceptions
 from skylark.compute.aws.aws_auth import AWSAuthentication
 from skylark.obj_store.object_store_interface import NoSuchObjectException, ObjectStoreInterface, ObjectStoreObject
 from skylark.utils import logger
 
-
 class S3Object(ObjectStoreObject):
     def full_path(self):
         return f"s3://{self.bucket}/{self.key}"
-
 
 class S3Interface(ObjectStoreInterface):
     def __init__(self, aws_region, bucket_name):
@@ -78,14 +75,63 @@ class S3Interface(ObjectStoreInterface):
         except NoSuchObjectException:
             return False
 
-    def download_object(self, src_object_name, dst_file_path):
+    def download_object(self, src_object_name, dst_file_path, byte_offset=None, byte_count=None):
+        assert len(src_object_name) > 0, f"Source object name must be non-empty: '{src_object_name}'"
         src_object_name, dst_file_path = str(src_object_name), str(dst_file_path)
         s3_client = self.auth.get_boto3_client("s3", self.aws_region)
-        assert len(src_object_name) > 0, f"Source object name must be non-empty: '{src_object_name}'"
-        s3_client.download_file(self.bucket_name, src_object_name, dst_file_path, Config=TransferConfig(use_threads=False))
+        if byte_offset is None or byte_count is None:
+            s3_client.download_file(self.bucket_name, src_object_name, dst_file_path)
+        else:
+            response = s3_client.get_object(
+                Bucket=self.bucket_name,
+                Key=src_object_name,
+                Range=f"bytes={byte_offset}-{byte_offset + byte_count - 1}"
+            )
+            if not os.path.exists(dst_file_path):
+                open(dst_file_path, "a").close()
+            with open(dst_file_path, "rb+") as f:
+                f.seek(byte_offset)
+                f.write(response["Body"].read())
+            response["Body"].close() 
 
-    def upload_object(self, src_file_path, dst_object_name):
+    def initiate_multipart_upload(self, dst_object_name, content_type):
+        #cannot infer content type here
+        assert len(dst_object_name) > 0, f"Destination object name must be non-empty: '{dst_object_name}'"
+        s3_client = self.auth.get_boto3_client("s3", self.aws_region)
+        response = s3_client.create_multipart_upload(
+            Bucket=self.bucket_name,
+            Key=dst_object_name,
+            ContentType=content_type
+        )
+        return response["UploadId"]
+
+    def upload_object(self, src_file_path, dst_object_name, upload_id=None, byte_offset=None, byte_count=None, part_number=None):
+        assert part_number is None or 1 <= part_number <= 10000, f"invalid part_number {part_number}, should be in range [1, 10000]" 
+        assert len(dst_object_name) > 0, f"Destination object name must be non-empty: '{dst_object_name}'"
         dst_object_name, src_file_path = str(dst_object_name), str(src_file_path)
         s3_client = self.auth.get_boto3_client("s3", self.aws_region)
+        if upload_id is None or byte_offset is None or byte_count is None or part_number is None:
+            s3_client.upload_file(src_file_path, self.bucket_name, dst_object_name)
+        else:
+            with open(src_file_path, mode="rb+") as f:
+                f.seek(byte_offset)
+                response = s3_client.upload_part(
+                    UploadId=upload_id,
+                    Bucket=self.bucket_name,
+                    Key=dst_object_name,
+                    PartNumber=part_number,
+                    Body=f,
+                    ContentLength=byte_count
+            )
+            return {"ETag": response["ETag"], "PartNumber": part_number} #user should build a list of these
+
+    def finalize_multipart_upload(self, dst_object_name, upload_id, part_list):
         assert len(dst_object_name) > 0, f"Destination object name must be non-empty: '{dst_object_name}'"
-        s3_client.upload_file(src_file_path, self.bucket_name, dst_object_name, Config=TransferConfig(use_threads=False))
+        part_list.sort(key=lambda d: d["PartNumber"]) #list sorting is handled here, not left to user
+        s3_client = self.auth.get_boto3_client("s3", self.aws_region)
+        response = s3_client.complete_multipart_upload(
+                UploadId=upload_id,
+                Bucket=self.bucket_name,
+                Key=dst_object_name,
+                MultipartUpload={"Parts": part_list}
+        )
