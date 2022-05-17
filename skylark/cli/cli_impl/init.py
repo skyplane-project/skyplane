e1fdbba512908b2609e4a53d0a34ac1ff58e4546
Paras Jain <parasj96@gmail.com>
2022-05-17 09:40:57 -0700

    Fix skylark init bugs (#350)
    * Skylark init error handling
    
    * Case handling for AWS init

--- skylark/cli/cli_impl/init.py
+++ skylark/cli/cli_impl/init.py
@@ -13,21 +13,28 @@ from skylark.compute.gcp.gcp_auth import GCPAuthentication
 def load_aws_config(config: SkylarkConfig) -> SkylarkConfig:
     # get AWS credentials from boto3
     session = boto3.Session()
-    credentials = session.get_credentials()
-    credentials = credentials.get_frozen_credentials()
-    auth = AWSAuthentication(config=config)
-    if credentials.access_key is None or credentials.secret_key is None:
+    credentials_session = session.get_credentials()
+    if credentials_session is None:
         config.aws_enabled = False
+    else:
+        credentials_frozen = credentials_session.get_frozen_credentials()
+        if credentials_frozen.access_key is None or credentials_frozen.secret_key is None:
+            config.aws_enabled = False
+        else:
+            config.aws_enabled = True
+
+    auth = AWSAuthentication(config=config)
+    if config.aws_enabled:
+        typer.secho(f"    Loaded AWS credentials from the AWS CLI [IAM access key ID: ...{credentials_frozen.access_key[-6:]}]", fg="blue")
+        config.aws_enabled = True
+        auth.save_region_config(config)
+        return config
+    else:
         typer.secho("    AWS credentials not found in boto3 session, please use the AWS CLI to set them via `aws configure`", fg="red")
         typer.secho("    https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html", fg="red")
         typer.secho("    Disabling AWS support", fg="blue")
         auth.clear_region_config()
         return config
-
-    typer.secho(f"    Loaded AWS credentials from the AWS CLI [IAM access key ID: ...{credentials.access_key[-6:]}]", fg="blue")
-    config.aws_enabled = True
-    auth.save_region_config(config)
-    return config
 
 
 def load_azure_config(config: SkylarkConfig, force_init: bool = False) -> SkylarkConfig:
--- skylark/cli/experiments/__init__.py
+++ skylark/cli/experiments/__init__.py
@@ -1,8 +1,7 @@
 import typer
 
 from skylark.cli.experiments.profile import latency_grid, throughput_grid
-from skylark.cli.experiments.util import get_max_throughput, util_grid_throughput, util_grid_cost, \
-    dump_full_util_cost_grid
+from skylark.cli.experiments.util import get_max_throughput, util_grid_throughput, util_grid_cost, dump_full_util_cost_grid
 
 app = typer.Typer(name="experiments")
 app.command()(latency_grid)
