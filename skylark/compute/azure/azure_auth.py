425a8b3d78c0642d780176830214182fd6f695b7
Paras Jain <parasj96@gmail.com>
2022-05-18 03:35:01 +0000

    Fix typing

--- skylark/compute/azure/azure_auth.py
+++ skylark/compute/azure/azure_auth.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import threading
-from typing import Optional
+from typing import Dict, List, Optional
 
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.authorization import AuthorizationManagementClient
@@ -35,17 +35,22 @@ try:
 except ImportError:
     ResourceManagementClient = None
 
+try:
+    from azure.mgmt.resource.subscriptions import SubscriptionClient
+except ImportError:
+    SubscriptionClient = None
+
 
 class AzureAuthentication:
     __cached_credentials = threading.local()
 
-    def __init__(self, config: Optional[SkylarkConfig] = None, subscription_id: str = cloud_config.azure_subscription_id):
-        if not config == None:
-            self.config = config
-        else:
-            self.config = SkylarkConfig.load_config(config_path)
-        self.subscription_id = subscription_id
-        self.credential = self.get_credential(subscription_id)
+    def __init__(self, config: Optional[SkylarkConfig] = None):
+        self.config = config if config is not None else SkylarkConfig.load_config(config_path)
+        self.credential = self.get_credential(self.subscription_id)
+
+    @property
+    def subscription_id(self) -> Optional[str]:
+        return self.config.azure_subscription_id
 
     def get_credential(self, subscription_id: str):
         cached_credential = getattr(self.__cached_credentials, f"credential_{subscription_id}", None)
@@ -62,11 +67,9 @@ class AzureAuthentication:
         if config.azure_enabled == False:
             self.clear_region_config()
             return
-        region_json = json.loads(subprocess.check_output(["az", "account", "list-locations", "-o", "json"]))
         region_list = []
-        for region in region_json:
-            region_list.append(region["name"])
-
+        for location in self.get_subscription_client().subscriptions.list_locations(subscription_id=self.subscription_id):
+            region_list.append(location.name)
         with azure_config_path.open("w") as f:
             f.write("\n".join(region_list))
         print(f"    Azure region config file saved to {azure_config_path}")
@@ -80,8 +83,8 @@ class AzureAuthentication:
                     valid_skus.append(sku.name)
             return set(valid_skus)
 
-        print("     Querying for SKU availbility in regions.")
-        result = do_parallel(get_skus, region_list, progress_bar=True, leave_pbar=False, desc="Query SKUs")
+        print("    Querying for SKU availbility in regions")
+        result = do_parallel(get_skus, region_list, spinner=True, spinner_persist=False, desc="Query SKUs")
 
         region_sku = dict()
         for region, skus in result:
@@ -91,16 +94,16 @@ class AzureAuthentication:
         with azure_sku_path.open("w") as f:
             json.dump(region_sku, f)
 
-        print(f"    Azure SKU availability cached in {azure_sku_path}.")
-
-
+        print(f"    Azure SKU availability cached in {azure_sku_path}")
 
     @staticmethod
     def get_region_config() -> List[str]:
         try:
             f = open(azure_config_path, "r")
         except FileNotFoundError:
-            print("     No Azure config detected! Consquently, the Azure region list is empty. Run skylark init --reinit-azure to remedy this.")
+            print(
+                "     No Azure config detected! Consquently, the Azure region list is empty. Run skylark init --reinit-azure to remedy this."
+            )
             return []
         region_list = []
         for region in f.read().split("\n"):
@@ -108,13 +111,13 @@ class AzureAuthentication:
         return region_list
 
     @staticmethod
-    def get_sku_mapping() -> dict[str, list[str]]:
+    def get_sku_mapping() -> Dict[str, List[str]]:
         try:
             f = open(azure_sku_path, "r")
         except FileNotFoundError:
             print("     Azure SKU data has not been chaced! Run skylark init to remedy this!")
             return dict()
-        
+
         return json.load(f)
 
     def clear_region_config(self):
@@ -144,6 +147,10 @@ class AzureAuthentication:
     def get_resource_client(self):
         assert ResourceManagementClient is not None, "ResourceManagementClient is not installed"
         return ResourceManagementClient(self.credential, self.subscription_id)
+
+    def get_subscription_client(self):
+        assert SubscriptionClient is not None, "SubscriptionClient is not installed"
+        return SubscriptionClient(self.credential)
 
     def get_network_client(self):
         assert NetworkManagementClient is not None, "NetworkManagementClient is not installed"
