7c9e58b8926725a2ef221ac84481c05736928223
Paras Jain <parasj96@gmail.com>
2022-05-18 18:50:43 -0700

    Fix GCP project configuration issues (#352)

--- skylark/compute/gcp/gcp_auth.py
+++ skylark/compute/gcp/gcp_auth.py
@@ -1,4 +1,3 @@
-import threading
 from typing import Optional
 
 import google.auth
@@ -11,31 +10,25 @@ from skylark.utils import logger
 
 
 class GCPAuthentication:
-    __cached_credentials = threading.local()
-
     def __init__(self, config: Optional[SkylarkConfig] = None, project_id: Optional[str] = cloud_config.gcp_project_id):
-        # load credentials lazily and then cache across threads
         if not config == None:
             self.config = config
         else:
             self.config = SkylarkConfig.load_config(config_path)
-        self.inferred_project_id = project_id
         self._credentials = None
-        self._project_id = None
 
-    def save_region_config(self, project_id=None):
-        project_id = project_id if project_id is not None else self.project_id
-        if project_id is None:
-            self.clear_region_config()
+    def save_region_config(self):
+        if self.project_id is None:
             print(
                 f"    No project ID detected when trying to save GCP region list! Consquently, the GCP region list is empty. Run 'skylark init --reinit-gcp' or file an issue to remedy this."
             )
+            self.clear_region_config()
             return
         with gcp_config_path.open("w") as f:
             region_list = []
             credentials = self.credentials
             service = discovery.build("compute", "beta", credentials=credentials)
-            request = service.zones().list(project=project_id)
+            request = service.zones().list(project=self.project_id)
             while request is not None:
                 response = request.execute()
                 # In reality, these are zones. However, we shall call them regions to be self-consistent.
@@ -47,7 +40,8 @@ class GCPAuthentication:
             f.write("\n".join(region_list))
             print(f"    GCP region config file saved to {gcp_config_path}")
 
-    def clear_region_config(self):
+    @staticmethod
+    def clear_region_config():
         with gcp_config_path.open("w") as f:
             f.write("")
 
@@ -63,26 +57,26 @@ class GCPAuthentication:
     @property
     def credentials(self):
         if self._credentials is None:
-            self._credentials, self._project_id = self.make_credential(self.inferred_project_id)
+            self._credentials, _ = self.get_adc_credential(self.project_id)
         return self._credentials
 
     @property
     def project_id(self):
-        if self._project_id is None:
-            self._credentials, self._project_id = self.make_credential(self.inferred_project_id)
-        return self._project_id
+        return self.config.gcp_project_id
 
-    def make_credential(self, project_id):
-        cached_credential = getattr(self.__cached_credentials, f"credential_{project_id}", (None, None))
-        if cached_credential == (None, None):
-            try:
-                inferred_cred, inferred_project = google.auth.default(quota_project_id=project_id)
-            except google.auth.exceptions.DefaultCredentialsError as e:
-                logger.error(f"Failed to load GCP credentials for project {project_id}: {e}")
-                inferred_cred, inferred_project = (None, None)
-            setattr(self.__cached_credentials, f"credential_{project_id}", (inferred_cred, project_id or inferred_project))
-            return inferred_cred, inferred_project
-        return cached_credential
+    @staticmethod
+    def get_adc_credential(project_id=None):
+        try:
+            inferred_cred, inferred_project = google.auth.default(quota_project_id=project_id)
+        except google.auth.exceptions.DefaultCredentialsError as e:
+            logger.error(f"Failed to load GCP credentials for project {project_id}: {e}")
+            inferred_cred, inferred_project = (None, None)
+        if project_id is not None and project_id != inferred_project:
+            logger.warning(
+                f"Google project ID error: Project ID from config {project_id} does not match inferred project from google.auth ADC {inferred_project}. Defaulting to config project."
+            )
+            inferred_project = project_id
+        return inferred_cred, inferred_project
 
     def enabled(self):
         return self.config.gcp_enabled and self.credentials is not None and self.project_id is not None
