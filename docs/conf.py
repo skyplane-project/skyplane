3e4b8cd0bd949eef0ccd11654fe98446b9733919
Paras Jain <parasj96@gmail.com>
2022-06-06 21:51:14 +0000

    Update base template for homepage

--- docs/conf.py
+++ docs/conf.py
@@ -78,17 +78,15 @@ exclude_patterns = ["build", "_build", "Thumbs.db", ".DS_Store"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# html_theme = "sphinx_book_theme"
 html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    #     'logo_only': True,
-    "show_toc_level": 2,
-}
+# html_theme_options = {
+#     "logo_only": True,
+#     "show_toc_level": 2,
+# }
 # html_logo = '_static/jax_logo_250px.png'
 # html_favicon = '_static/favicon.png'
 # html_static_path = ['_static']
