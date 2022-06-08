736ff7f8bb311ed3130fc4cd4412ca0867276c6a
Paras Jain <parasj96@gmail.com>
2022-06-08 03:06:27 +0000

    Show transfer ETA instead of elapsed time

--- skyplane/replicate/replicator_client.py
+++ skyplane/replicate/replicator_client.py
@@ -7,7 +7,7 @@ from functools import partial
 from typing import Dict, List, Optional, Tuple, Iterable
 
 import pandas as pd
-from rich.progress import Progress, SpinnerColumn, TextColumn, TimeElapsedColumn, DownloadColumn, BarColumn, TransferSpeedColumn
+from rich.progress import Progress, SpinnerColumn, TextColumn, TimeRemainingColumn, DownloadColumn, BarColumn, TransferSpeedColumn
 
 from skyplane import GB, MB, tmp_log_dir
 from skyplane.chunk import Chunk, ChunkRequest, ChunkState
@@ -486,7 +486,7 @@ class ReplicatorClient:
                 BarColumn(),
                 DownloadColumn(binary_units=True),
                 TransferSpeedColumn(),
-                TimeElapsedColumn(),
+                TimeRemainingColumn(),
                 disable=not show_spinner,
             ) as progress:
                 copy_task = progress.add_task("", total=total_bytes)
