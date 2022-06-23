653d8578a67642248814fde53f20fbec1ca766b9
Paras Jain <parasj96@gmail.com>
2022-06-23 19:14:42 +0000

    format code

--- tests/unit_nocloud/test_cp_replicate.py
+++ tests/unit_nocloud/test_cp_replicate.py
@@ -9,7 +9,7 @@ def check_exception_raised(func, exception_type, exception_msg=None):
     except exception_type as e:
         if exception_msg is not None:
             assert exception_msg in str(e)
-        
+
 
 def test_map_object_single_file():
     assert map_object_key_prefix("foo/a.txt", "foo/a.txt", "bar") == "bar"
@@ -18,11 +18,16 @@ def test_map_object_single_file():
     check_exception_raised(lambda: map_object_key_prefix("foo/", "bar", "foo/a.txt", "bar"), exceptions.MissingObjectException)
     check_exception_raised(lambda: map_object_key_prefix("foo", "foo/a.txt", "bar"), exceptions.MissingObjectException)
 
+
 def test_map_object_recursive():
     assert map_object_key_prefix("foo/", "foo/a.txt", "bar", recursive=True) == "bar/a.txt"
     assert map_object_key_prefix("foo/", "foo/b.txt", "bar", recursive=True) == "bar/b.txt"
     assert map_object_key_prefix("foo", "foo/c.txt", "bar", recursive=True) == "bar/c.txt"
     check_exception_raised(lambda: map_object_key_prefix("foo", "fooc.txt", "bar", recursive=True), exceptions.MissingObjectException)
-    check_exception_raised(lambda: map_object_key_prefix("foo/a.txt", "foo/a.txt", "bar", recursive=True), exceptions.MissingObjectException)
-    check_exception_raised(lambda: map_object_key_prefix("foo/a.txt", "foo/b.txt", "bar/", recursive=True), exceptions.MissingObjectException)
+    check_exception_raised(
+        lambda: map_object_key_prefix("foo/a.txt", "foo/a.txt", "bar", recursive=True), exceptions.MissingObjectException
+    )
+    check_exception_raised(
+        lambda: map_object_key_prefix("foo/a.txt", "foo/b.txt", "bar/", recursive=True), exceptions.MissingObjectException
+    )
     assert map_object_key_prefix("bar/foo/", "bar/foo/b.txt", "bar", recursive=True) == "bar/b.txt"
