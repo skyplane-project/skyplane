ebf23dc600b12f882900f39d61ddba302ff8147e
Paras Jain <parasj96@gmail.com>
2022-08-09 20:21:34 -0700

    Update

--- docs/conf.py
+++ docs/conf.py
@@ -84,10 +84,10 @@ html_theme = "furo"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "logo_only": True,
-    "show_toc_level": 2,
-}
+# html_theme_options = {
+#     "logo_only": True,
+#     "show_toc_level": 2,
+# }
 html_logo = "images/logo.png"
 # html_favicon = '_static/favicon.png'
 html_static_path = ["images"]
