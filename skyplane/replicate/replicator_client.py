fb3d42250eedc8810b813e92a17aff3393f4bff4
Paras Jain <parasj96@gmail.com>
2022-09-05 17:58:26 +0000

    Clean up logging

--- skyplane/replicate/replicator_client.py
+++ skyplane/replicate/replicator_client.py
@@ -368,10 +368,9 @@ class ReplicatorClient:
                     idx += 1
 
             # initiate multipart transfers in parallel
-            progress.update(prepare_task, description=": Initiating multipart transfers")
+            progress.update(prepare_task, description=f": Queuing {len(multipart_pairs)} files for multipart transfers")
             obj_store_interface = ObjectStoreInterface.create(job.dest_region, job.dest_bucket)
             upload_ids = do_parallel(lambda x: obj_store_interface.initiate_multipart_upload(x[1].key), multipart_pairs, n=256)
-            progress.update(prepare_task, description=": Queuing files for multipart transfer")
             for (src_object, dest_object), upload_id in upload_ids:
                 # determine number of chunks via the following algorithm:
                 chunk_size_bytes = int(multipart_min_size_mb * MB)
