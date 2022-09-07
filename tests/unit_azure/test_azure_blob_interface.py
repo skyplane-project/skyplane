7476c5e8aa1a247d62786759fab7d0397f2689e1
Paras Jain <parasj96@gmail.com>
2022-09-07 21:06:27 +0000

    Update

--- tests/unit_azure/test_azure_blob_interface.py
+++ tests/unit_azure/test_azure_blob_interface.py
@@ -4,8 +4,8 @@ from tests.interface_util import interface_test_framework
 from skyplane.utils import logger
 
 
-def test_azure_singlepart():
-    assert interface_test_framework("azure:eastus", "sky-us-east-1", False)
+# def test_azure_singlepart():
+#     assert interface_test_framework("azure:eastus", "sky-us-east-1", False)
 
 
 def test_azure_multipart():
@@ -14,13 +14,15 @@ def test_azure_multipart():
 
 
 def test_azure_bucket_exists():
-    # test a random bucket that doesn't exist
-    iface = ObjectStoreInterface.create("azure:infer", f"skyplane-does-not-exist-{uuid.uuid4()}/{uuid.uuid4()}")
-    assert not iface.bucket_exists()
-
     # test a public bucket with objects
     iface = ObjectStoreInterface.create("azure:infer", "azureopendatastorage/mnist")
+    assert not iface.storage_account_interface.storage_account_exists_in_account()  # this is a public bucket so is not owned by the user's account
     assert iface.bucket_exists()
+
+    # test a random bucket that doesn't exist
+    iface = ObjectStoreInterface.create("azure:infer", f"skyplane-does-not-exist-{uuid.uuid4()}/{uuid.uuid4()}")
+    assert not iface.storage_account_interface.storage_account_exists_in_account()
+    assert not iface.bucket_exists()
 
     # test public but empty bucket
     # iface = ObjectStoreInterface.create("aws:infer", "skyplane-test-empty-public-bucket")
