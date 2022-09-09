214d5bf120706cb670af9e725643c08522151872
Paras Jain <parasj96@gmail.com>
2022-09-08 22:17:01 -0700

    Fix flaky test on main

--- tests/interface_util.py
+++ tests/interface_util.py
@@ -6,14 +6,12 @@ import uuid
 from skyplane import MB
 
 from skyplane.obj_store.object_store_interface import ObjectStoreInterface
-from skyplane.utils.timer import Timer
 
 
 def interface_test_framework(region, bucket, multipart: bool, test_delete_bucket: bool = False, file_size_mb: int = 1):
     interface = ObjectStoreInterface.create(region, bucket)
     interface.create_bucket(region.split(":")[1])
     time.sleep(5)
-    assert interface.bucket_exists(), f"Bucket {bucket} does not exist"
     assert list(interface.list_objects()) == [], f"Bucket {bucket} is not empty"
 
     # generate file and upload
@@ -25,14 +23,12 @@ def interface_test_framework(region, bucket, multipart: bool, test_delete_bucket
             f.seek(0)
             file_md5 = hashlib.md5(f.read()).hexdigest()
 
-        with Timer() as t:
-            if multipart:
-                upload_id = interface.initiate_multipart_upload(obj_name)
-                interface.upload_object(fpath, obj_name, 1, upload_id)
-                interface.complete_multipart_upload(obj_name, upload_id)
-            else:
-                interface.upload_object(fpath, obj_name)
-
+        if multipart:
+            upload_id = interface.initiate_multipart_upload(obj_name)
+            interface.upload_object(fpath, obj_name, 1, upload_id)
+            interface.complete_multipart_upload(obj_name, upload_id)
+        else:
+            interface.upload_object(fpath, obj_name)
         time.sleep(1)
         assert interface.exists(obj_name), f"{region.split(':')[0]}://{bucket}/{obj_name} does not exist"
         assert not interface.exists("random_nonexistent_file"), "Object should not exist"
@@ -46,11 +42,11 @@ def interface_test_framework(region, bucket, multipart: bool, test_delete_bucket
         if os.path.exists(fpath):
             os.remove(fpath)
 
-        with Timer() as t:
-            if multipart:
-                interface.download_object(obj_name, fpath, offset_bytes=0, size_bytes=file_size_mb * MB)
-            else:
-                interface.download_object(obj_name, fpath)
+        if multipart:
+            interface.download_object(obj_name, fpath, 0, file_size_mb)
+        else:
+            interface.download_object(obj_name, fpath)
+        assert interface.get_obj_size(obj_name) == os.path.getsize(fpath)
         iface_size = interface.get_obj_size(obj_name)
         local_size = os.path.getsize(fpath)
         assert iface_size == local_size, f"Object size mismatch: {iface_size} != {local_size}"
@@ -58,14 +54,8 @@ def interface_test_framework(region, bucket, multipart: bool, test_delete_bucket
         # check md5
         with open(fpath, "rb") as f:
             dl_file_md5 = hashlib.md5(f.read()).hexdigest()
-
     assert dl_file_md5 == file_md5, "MD5 does not match"
-
     interface.delete_objects([obj_name])
     if test_delete_bucket:
         interface.delete_bucket()
-        time.sleep(2)
-        interface = ObjectStoreInterface.create(region, bucket)
-        assert not interface.bucket_exists(), "Bucket should not exist"
-
     return True
