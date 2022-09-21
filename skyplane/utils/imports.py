5dab6602f602dc87e156762c3b0859f04bf535b1
Paras Jain <parasj96@gmail.com>
2022-09-21 16:07:00 -0700

    Update import logic to import member from module

--- skyplane/utils/imports.py
+++ skyplane/utils/imports.py
@@ -14,14 +14,23 @@ def inject(*modules, pip_extra=None):
         @functools.wraps(fn)
         def wrapped(*args, **kwargs):
             modules_imported = []
+            err_msg = f"Cannot import {module}."
+            if pip_extra:
+                err_msg += f" Install skyplane with {pip_extra} support: `pip install skyplane[{pip_extra.lower()}]`"
             for module in modules:
                 try:
                     modules_imported.append(importlib.import_module(module))
                 except ImportError as e:
-                    msg = f"Cannot import {module}."
-                    if pip_extra:
-                        msg += f" Install skyplane with {pip_extra} support: `pip install skyplane[{pip_extra.lower()}]`"
-                    raise ImportError(msg) from e
+                    # try the "from x import y" syntax
+                    module_split = module.split(".")
+                    if len(module_split) > 1:
+                        try:
+                            module = importlib.import_module(".".join(module_split[:-1]))
+                            modules_imported.append(getattr(module, module_split[-1]))
+                        except (ImportError, AttributeError):
+                            raise ImportError(err_msg) from e
+                    else:
+                        raise ImportError(err_msg) from e
             return fn(*modules_imported, *args, **kwargs)
 
         return wrapped
