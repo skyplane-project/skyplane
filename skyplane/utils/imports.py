1ab511b4a9dfcabf10dbcc08e306373aae1211a7
Paras Jain <parasj96@gmail.com>
2022-09-21 16:11:12 -0700

    Fix message

--- skyplane/utils/imports.py
+++ skyplane/utils/imports.py
@@ -14,19 +14,18 @@ def inject(*modules, pip_extra=None):
         @functools.wraps(fn)
         def wrapped(*args, **kwargs):
             modules_imported = []
-            err_msg = f"Cannot import {module}."
-            if pip_extra:
-                err_msg += f" Install skyplane with {pip_extra} support: `pip install skyplane[{pip_extra.lower()}]`"
             for module in modules:
+                err_msg = f"Cannot import {module}."
+                if pip_extra:
+                    err_msg += f" Install skyplane with {pip_extra} support: `pip install skyplane[{pip_extra.lower()}]`"
                 try:
                     modules_imported.append(importlib.import_module(module))
                 except ImportError as e:
-                    # try the "from x import y" syntax
                     module_split = module.split(".")
-                    if len(module_split) > 1:
+                    if len(module_split) > 1:  # try the "from x import y" syntax
                         try:
-                            module = importlib.import_module(".".join(module_split[:-1]))
-                            modules_imported.append(getattr(module, module_split[-1]))
+                            module_imported = importlib.import_module(".".join(module_split[:-1]))
+                            modules_imported.append(getattr(module_imported, module_split[-1]))
                         except (ImportError, AttributeError):
                             raise ImportError(err_msg) from e
                     else:
