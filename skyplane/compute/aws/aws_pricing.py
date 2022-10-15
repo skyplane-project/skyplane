a503c1552c4af1ddaf1408dbf237bf44d05a9150
Paras Jain <parasj96@gmail.com>
2022-10-15 12:44:31 -0700

    Fix pytype

--- skyplane/compute/aws/aws_pricing.py
+++ skyplane/compute/aws/aws_pricing.py
@@ -23,20 +23,21 @@ class AWSPricing:
 
     def get_transfer_cost(self, src_key, dst_key, premium_tier=True):
         assert premium_tier, "AWS transfer cost is only available for premium tier"
-        if self.transfer_df is None:
+        transfer_df = self.transfer_df
+        if transfer_df is None:
             return None
-
-        src_provider, src = src_key.split(":")
-        dst_provider, dst = dst_key.split(":")
-
-        assert src_provider == "aws"
-        if dst_provider == "aws":
-            if (src, dst) in self.transfer_df.index:
-                return self.transfer_df.loc[src, dst]["cost"]
-            else:
-                logger.warning(f"No transfer cost found for {src_key} -> {dst_key}, using max of {src}")
-                src_rows = self.transfer_df.loc[src]
-                src_rows = src_rows[src_rows.index != "internet"]
-                return src_rows.max()["cost"]
         else:
-            return self.transfer_df.loc[src, "internet"]["cost"]
+            src_provider, src = src_key.split(":")
+            dst_provider, dst = dst_key.split(":")
+
+            assert src_provider == "aws"
+            if dst_provider == "aws":
+                if (src, dst) in transfer_df.index:
+                    return transfer_df.loc[src, dst]["cost"]
+                else:
+                    logger.warning(f"No transfer cost found for {src_key} -> {dst_key}, using max of {src}")
+                    src_rows = transfer_df.loc[src]
+                    src_rows = src_rows[src_rows.index != "internet"]
+                    return src_rows.max()["cost"]
+            else:
+                return transfer_df.loc[src, "internet"]["cost"]
