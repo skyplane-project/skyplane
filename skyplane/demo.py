7f1fcea7494e693cd6565745ee714b2741ba386d
Jason Ding <zhoujie.ding@berkeley.edu>
2022-10-18 21:32:26 +0000

    update demo

--- skyplane/demo.py
+++ skyplane/demo.py
@@ -1,8 +1,13 @@
 from skyplane.api.api_class import *
+import time
+
+# preprovision vms for demo
+# keep the logging
 
 # Simple Copy
-# client1 = new_client(None)
-# client1.copy(src="s3://jason-us-east-1/fake_imagenet/", dst="s3://jason-us-west-2/fake_imagenet_1/", recursive=True)
+client1 = new_client(None)
+client1.copy(src="s3://jason-us-east-1/imagenet-train-000000.tar", 
+            dst="s3://jason-us-west-2/imagenet-train-000000.tar", recursive=False)
 
 # Session Copy
 client2 = new_client(None)
@@ -14,14 +19,18 @@ session = client2.new_session(
 )
 
 with session as s:
-    # session.auto_terminate()
+    session.auto_terminate()
     s.copy("fake_imagenet", "fake_imagenet_test", recursive=True)
-    s.copy("fake_imagenet", "fake_imagenet_rand", recursive=True)
+    # s.copy("fake_imagenet", "fake_imagenet_rand", recursive=True)
     # This will not start the transfer
     future1 = s.run_async()
+    s.wait_for_completion(future1)
     s.copy("fake_imagenet", "fake_imagenet_2", recursive=True)
     # This will not start the transfer
+    start_time = time.time()
     future2 = s.run_async()
+    print("future 2", future2.running())
+    print("spent", time.time()-start_time)
     # This starts the transfer
-    s.run(future2)
-    s.run(future1)
+    # s.run(future2)
+    # s.run(future1)
