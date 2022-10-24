3524bae81b5b1395a46c16d9f0a4401c765fe589
Paras Jain <parasj96@gmail.com>
2022-10-24 00:17:10 -0700

    Merge start gateways with provision logic

--- skyplane/api/dataplane.py
+++ skyplane/api/dataplane.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from functools import partial
+import threading
 from typing import Dict, Optional
 
 import nacl.secret
@@ -9,7 +10,19 @@ from skyplane import gateway_docker_image
 from skyplane.api.provisioner import Provisioner
 from skyplane.compute.server import Server
 from skyplane.replicate.replication_plan import ReplicationTopology, ReplicationTopologyGateway
+from skyplane.utils import logger
 from skyplane.utils.fn import do_parallel
+
+
+class DataplaneAutoDeprovision:
+    def __init__(self, dataplane: "Dataplane"):
+        self.dataplane = dataplane
+
+    def __enter__(self):
+        return self.dataplane
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.dataplane.deprovision()
 
 
 class Dataplane:
@@ -23,6 +36,8 @@ class Dataplane:
     ):
         self.topology = topology
         self.provisioner = provisioner
+        self.provisioning_lock = threading.Lock()
+        self.provisioned = False
 
         # config parameters
         self.config = {
@@ -49,47 +64,75 @@ class Dataplane:
 
     def provision(
         self,
+        # provisioning options
         allow_firewall: bool = True,
+        # gateway options
+        gateway_docker_image: str = gateway_docker_image(),
+        log_dir: Optional[str] = None,
+        authorize_ssh_pub_key: Optional[str] = None,
         max_jobs: int = 16,
         spinner: bool = False,
     ):
-        aws_nodes_to_provision = list(n.region.split(":")[0] for n in self.topology.nodes if n.region.startswith("aws:"))
-        azure_nodes_to_provision = list(n.region.split(":")[0] for n in self.topology.nodes if n.region.startswith("azure:"))
-        gcp_nodes_to_provision = list(n.region.split(":")[0] for n in self.topology.nodes if n.region.startswith("gcp:"))
+        with self.provisioning_lock:
+            if self.provisioned:
+                logger.error("Cannot provision dataplane, already provisioned!")
+                return
+            aws_nodes_to_provision = list(n.region.split(":")[0] for n in self.topology.nodes if n.region.startswith("aws:"))
+            azure_nodes_to_provision = list(n.region.split(":")[0] for n in self.topology.nodes if n.region.startswith("azure:"))
+            gcp_nodes_to_provision = list(n.region.split(":")[0] for n in self.topology.nodes if n.region.startswith("gcp:"))
 
-        # create VMs from the topology
-        for node in self.topology.nodes:
-            cloud_provider, region = node.region.split(":")
-            self.provisioner.add_task(
-                cloud_provider=cloud_provider,
-                region=region,
-                vm_type=self.config[f"{cloud_provider}_instance_class"],
-                spot=self.config.get(f"{cloud_provider}_use_spot_instances", False),
-                autoterminate_minutes=self.config.get("autoterminate_minutes", None),
+            # create VMs from the topology
+            for node in self.topology.nodes:
+                cloud_provider, region = node.region.split(":")
+                self.provisioner.add_task(
+                    cloud_provider=cloud_provider,
+                    region=region,
+                    vm_type=self.config[f"{cloud_provider}_instance_class"],
+                    spot=self.config.get(f"{cloud_provider}_use_spot_instances", False),
+                    autoterminate_minutes=self.config.get("autoterminate_minutes", None),
+                )
+
+            # initialize clouds
+            self.provisioner.init_global(
+                aws=len(aws_nodes_to_provision) > 0,
+                azure=len(azure_nodes_to_provision) > 0,
+                gcp=len(gcp_nodes_to_provision) > 0,
             )
 
-        # initialize clouds
-        self.provisioner.init_global(
-            aws=len(aws_nodes_to_provision) > 0,
-            azure=len(azure_nodes_to_provision) > 0,
-            gcp=len(gcp_nodes_to_provision) > 0,
-        )
+            # provision VMs
+            uuids = self.provisioner.provision(
+                allow_firewall=allow_firewall,
+                max_jobs=max_jobs,
+                spinner=spinner,
+            )
 
-        # provision VMs
-        uuids = self.provisioner.provision(
-            allow_firewall=allow_firewall,
-            max_jobs=max_jobs,
-            spinner=spinner,
-        )
+            # bind VMs to nodes
+            servers = [self.provisioner.get_node(u) for u in uuids]
+            servers_by_region = defaultdict(list)
+            for s in servers:
+                servers_by_region[s.region_tag].append(s)
+            for node in self.topology.gateway_nodes:
+                instance = servers_by_region[node.region].pop()
+                self.bound_nodes[node] = instance
 
-        # bind VMs to nodes
-        servers = [self.provisioner.get_node(u) for u in uuids]
-        servers_by_region = defaultdict(list)
-        for s in servers:
-            servers_by_region[s.region_tag].append(s)
-        for node in self.topology.gateway_nodes:
-            instance = servers_by_region[node.region].pop()
-            self.bound_nodes[node] = instance
+            # todo: move server.py:start_gateway here
+            # generate end-to-end key and configure gateway start script
+            e2ee_key_bytes = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+            start_fn = partial(
+                self._start_gateway,
+                gateway_docker_image=gateway_docker_image,
+                log_dir=log_dir,
+                authorize_ssh_pub_key=authorize_ssh_pub_key,
+                e2ee_key_bytes=e2ee_key_bytes,
+            )
+
+            # start gateways
+            jobs = []
+            for node, server in self.bound_nodes.items():
+                jobs.append(partial(start_fn, node, server))
+            do_parallel(lambda fn: fn(), jobs, n=max_jobs, spinner=spinner, spinner_persist=True, desc="Starting gateway container on VMs")
+
+            self.provisioned = True
 
     def _start_gateway(
         self,
@@ -128,33 +171,16 @@ class Dataplane:
             use_socket_tls=self.config.get("use_socket_tls", False),
         )
 
-    def start_gateways(
-        self,
-        gateway_docker_image: str = gateway_docker_image(),
-        log_dir: Optional[str] = None,
-        authorize_ssh_pub_key: Optional[str] = None,
-        max_jobs: int = 16,
-        spinner: bool = False,
-    ):
-        # todo: move server.py:start_gateway here
-        # generate end-to-end key and configure gateway start script
-        e2ee_key_bytes = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
-        start_fn = partial(
-            self._start_gateway,
-            gateway_docker_image=gateway_docker_image,
-            log_dir=log_dir,
-            authorize_ssh_pub_key=authorize_ssh_pub_key,
-            e2ee_key_bytes=e2ee_key_bytes,
-        )
-
-        # start gateways
-        jobs = []
-        for node, server in self.bound_nodes.items():
-            jobs.append(partial(start_fn, node, server))
-        do_parallel(lambda fn: fn(), jobs, n=max_jobs, spinner=spinner, spinner_persist=True, desc="Starting gateway container on VMs")
-
     def deprovision(self, max_jobs: int = 64, spinner: bool = False):
-        self.provisioner.deprovision(
-            max_jobs=max_jobs,
-            spinner=spinner,
-        )
+        with self.provisioning_lock:
+            if not self.provisioned:
+                logger.warning("Attempting to deprovision dataplane that is not provisioned")
+            self.provisioner.deprovision(
+                max_jobs=max_jobs,
+                spinner=spinner,
+            )
+            self.provisioned = False
+
+    def auto_deprovision(self) -> DataplaneAutoDeprovision:
+        """Returns a context manager that will automatically call deprovision upon exit."""
+        return DataplaneAutoDeprovision(self)
