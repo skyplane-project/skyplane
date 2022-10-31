d17fa6815d15b10e17d5a3b455d80b97f87d69e8
Paras Jain <parasj96@gmail.com>
2022-10-31 16:04:25 -0700

    [draft] Remove setup.py with editable support (#640)
    Poetry now supports [editable
    installation](https://github.com/python-poetry/poetry/issues/34#issuecomment-386134776),
    so this PR tests removing `setup.py`.

--- setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup(
-        packages=["skyplane"], entry_points={"console_scripts": ["skylark=skyplane.cli.cli:app", "skyplane=skyplane.cli.cli:app"]}
-    )
