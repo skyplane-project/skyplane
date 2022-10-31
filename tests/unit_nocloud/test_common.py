1eb0aae0c16cac5be981f1aabdbbd7939a65ccd2
Paras Jain <parasj96@gmail.com>
2022-10-31 09:49:37 -0700

    Fix broken Azure path logic (#650)
    Fixes an issue reported by Ajay Rana
    
    Azure paths were not parsed correctly, such as
    `azure://account/container`.

---
+++ tests/unit_nocloud/test_common.py
@@ -0,0 +1,31 @@
+import pytest
+import typer
+from skyplane.cli.common import parse_path
+
+
+def test_parse_path():
+    # test local
+    assert parse_path("/") == ("local", None, "/")
+    assert parse_path("/tmp") == ("local", None, "/tmp")
+    assert parse_path("does-not-exist-0000000/file") == ("local", None, "does-not-exist-0000000/file")
+
+    # test s3://
+    assert parse_path("s3://bucket") == ("aws", "bucket", "")
+    assert parse_path("s3://bucket/") == ("aws", "bucket", "")
+    assert parse_path("s3://bucket/key") == ("aws", "bucket", "key")
+
+    # test gs://
+    assert parse_path("gs://bucket") == ("gcp", "bucket", "")
+    assert parse_path("gs://bucket/") == ("gcp", "bucket", "")
+    assert parse_path("gs://bucket/key") == ("gcp", "bucket", "key")
+
+    # test https:// azure
+    assert parse_path("https://bucket.blob.core.windows.net/container") == ("azure", "bucket/container", "")
+    assert parse_path("https://bucket.blob.core.windows.net/container/") == ("azure", "bucket/container", "")
+    assert parse_path("https://bucket.blob.core.windows.net/container/key") == ("azure", "bucket/container", "key")
+
+    # test azure:// azure
+    assert parse_path("azure://bucket/container") == ("azure", "bucket/container", "")
+    assert parse_path("azure://bucket/container/") == ("azure", "bucket/container", "")
+    assert parse_path("azure://bucket/container/key") == ("azure", "bucket/container", "key")
+    assert parse_path("azure://bucket/container/key/path") == ("azure", "bucket/container", "key/path")
