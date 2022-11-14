e3d1bd66e5f2af457825667ba59a82c89f31f719
Jason <zhoujie.ding@berkeley.edu>
2022-11-14 13:34:51 -0800

    reformat code

--- examples/pytorch_training.py
+++ examples/pytorch_training.py
@@ -21,8 +21,8 @@ from PIL import Image  # type: ignore
 
 class ImageNetS3(IterableDataset):
     def __init__(self, url_list, shuffle_urls=True):
-            self.s3_iter_dataset = S3IterableDataset(url_list, shuffle_urls)
-            self.transform = transforms.Compose(
+        self.s3_iter_dataset = S3IterableDataset(url_list, shuffle_urls)
+        self.transform = transforms.Compose(
             [
                 transforms.RandomResizedCrop(224),
                 transforms.RandomHorizontalFlip(),
@@ -60,14 +60,14 @@ if __name__ == "__main__":
     #           to download the dataset to my local machine. AWS s3 cp runs at 20MB/s :(
     # Solution: Use the Skyplane API to copy the dataset to an S3 bucket in the same region as my GPUs.
     #           Then we can use the S3IterableDataset to read the data directly from S3 at high speeds.
-    #           This example uses the ImageNet dataset, but it can be used with any dataset stored in S3. 
-    
+    #           This example uses the ImageNet dataset, but it can be used with any dataset stored in S3.
+
     # Step 0: My data is in a remote S3 bucket and directly reading it is too slow :(
     parser = argparse.ArgumentParser()
     parser.add_argument("--remote-s3-path", type=str, default="s3://jason-us-east-1/data/")
     parser.add_argument("--local-s3-path", type=str, default="s3://jason-us-west-2/data/")
     args = parser.parse_args()
-    
+
     # Step 1:  Create a Skyplane API client. It will read your AWS credentials from the AWS CLI by default
     client = skyplane.SkyplaneClient(aws_config=skyplane.AWSConfig())
 
@@ -75,15 +75,18 @@ if __name__ == "__main__":
     client.copy(src=args.remote_s3_path, dst=args.local_s3_path, recursive=True)
 
     # Step 3:  Create a dataloader from the local bucket. This will read the data directly from S3 at high speeds!
-    data_urls = [(args.local_s3_path + "/" if not args.local_s3_path.endswith("/") else args.local_s3_path) + f"imagenet-train-{i:06d}.tar" for i in range(100)]
+    data_urls = [
+        (args.local_s3_path + "/" if not args.local_s3_path.endswith("/") else args.local_s3_path) + f"imagenet-train-{i:06d}.tar"
+        for i in range(100)
+    ]
     dataset = ImageNetS3(data_urls)
     train_loader = DataLoader(dataset, batch_size=256, num_workers=2)
-    
+
     # Step 4:  Train a model!
     model = models.__dict__["resnet18"](pretrained=True)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), 0.1, momentum=0.9, weight_decay=1e-4)
-    
+
     model.train()
     for ep in range(5):
         print(f"Start training epoch {ep}")
