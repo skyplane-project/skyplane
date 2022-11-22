42d2f44b4de59735ee58fbc25736e8c7c2a4b30a
Hailey Jang <hailey.jang.01@gmail.com>
2022-11-21 18:00:48 -0800

    Make some changes to arguments

--- scripts/on_prem/benchmark.py
+++ scripts/on_prem/benchmark.py
@@ -2,6 +2,7 @@ import os
 import time
 from pyarrow import fs
 import threading
+import argparse
 
 KB = 1024
 def transfer_file(in_fs, in_path, out_fs, out_path, BATCH_SIZE):
@@ -24,31 +25,31 @@ def transfer_file(in_fs, in_path, out_fs, out_path, BATCH_SIZE):
                 
     print(f"Time taken to copy 100 125MB files from local to HDFS for {BATCH_SIZE/KB}KB: {time.time() - before}")
                 
-def setup_files_and_dirs():
+def setup_files_and_dirs(outdir):
     #setup 10GB file
-    os.system(f"dd if=/dev/zero of=data/10GBdata.bin bs=10GB count=1")
+    os.mkdir(f"{outdir}")
+    os.system(f"dd if=/dev/zero of={outdir}/10GBdata.bin bs=128KB count=78125")
     
-                
-def transfer_local_to_hdfs(hdfs, local):
+def transfer_local_to_hdfs(hdfs, local, outdir):
     #32/64/128/156 KBs
-    transfer_file(local, f"data/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 32*KB)
+    transfer_file(local, f"{outdir}/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 32*KB)
     
-    transfer_file(local, f"data/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 64*KB)
+    transfer_file(local, f"{outdir}/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 64*KB)
 
-    transfer_file(local, f"data/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 128*KB)
+    transfer_file(local, f"{outdir}/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 128*KB)
 
-    transfer_file(local, f"data/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 156*KB)
+    transfer_file(local, f"{outdir}/10GBdata.bin", hdfs, f"/data/10GBdata.bin", 156*KB)
 
     
-def transfer_hdfs_to_local(hdfs, local):
+def transfer_hdfs_to_local(hdfs, local, outdir):
     #32/64/128/156 KBs
-    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"data/10GBdata.bin", 32*KB)
+    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"{outdir}/10GBdata.bin", 32*KB)
     
-    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"data/10GBdata.bin", 64*KB)
+    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"{outdir}/10GBdata.bin", 64*KB)
     
-    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"data/10GBdata.bin", 128*KB)
+    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"{outdir}/10GBdata.bin", 128*KB)
     
-    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"data/10GBdata.bin", 156*KB)
+    transfer_file(hdfs, f"/data/10GBdata.bin", local, f"{outdir}/10GBdata.bin", 156*KB)
 
 
 def parallel_reads(hdfs):
@@ -56,11 +57,15 @@ def parallel_reads(hdfs):
     
     
 if __name__ == "__main__":
-    hdfs = fs.HadoopFileSystem(host="ec2-18-222-145-65.us-east-2.compute.amazonaws.com", port=8020, extra_conf={'dfs.client.use.datanode.hostname': 'false'})
+    parser = argparse.ArgumentParser()
+    parser.add_argument('HDFS', type=str, help="HDFS host")
+    parser.add_argument("--outdir", type=str, default="/tmp/data")
+    args = parser.parse_args()
+    
+    hdfs = fs.HadoopFileSystem(host=args.HDFS, port=8020, extra_conf={'dfs.client.use.datanode.hostname': 'false'})
     local = fs.LocalFileSystem()
 
-
-    setup_files_and_dirs()
+    setup_files_and_dirs(args.outdir)
     transfer_local_to_hdfs(hdfs, local)
     transfer_hdfs_to_local(hdfs, local)
     
