e5fb1c69a55451daa77dbb9b74a04ab431d2f8d2
Sarah Wooders <sarahwooders@gmail.com>
2022-11-21 23:19:07 -0800

    add back gateway

---
+++ skyplane/gateway/gateway_daemon.py
@@ -0,0 +1,195 @@
+import argparse
+import atexit
+import json
+import os
+import signal
+import sys
+import threading
+import time
+from multiprocessing import Event, Queue
+from os import PathLike
+from pathlib import Path
+from threading import BoundedSemaphore
+
+from typing import Dict
+
+from skyplane.chunk import ChunkState
+from skyplane.gateway.chunk_store import ChunkStore
+from skyplane.gateway.gateway_daemon_api import GatewayDaemonAPI
+from skyplane.gateway.gateway_obj_store import GatewayObjStoreConn
+from skyplane.gateway.gateway_receiver import GatewayReceiver
+from skyplane.gateway.gateway_sender import GatewaySender
+from skyplane.utils import logger
+from skyplane.utils.definitions import MB
+
+
+class GatewayDaemon:
+    def __init__(
+        self,
+        region: str,
+        outgoing_ports: Dict[str, int],
+        chunk_dir: PathLike,
+        max_incoming_ports=64,
+        use_tls=True,
+        use_compression=False,
+        use_e2ee=True,
+    ):
+        # todo max_incoming_ports should be configurable rather than static
+        self.region = region
+        self.max_incoming_ports = max_incoming_ports
+        self.chunk_store = ChunkStore(chunk_dir)
+        self.error_event = Event()
+        self.error_queue = Queue()
+        if use_e2ee:
+            e2ee_key_path = Path(os.environ["E2EE_KEY_FILE"]).expanduser()
+            with open(e2ee_key_path, "rb") as f:
+                e2ee_key_bytes = f.read()
+        else:
+            e2ee_key_bytes = None
+        self.gateway_receiver = GatewayReceiver(
+            region,
+            self.chunk_store,
+            self.error_event,
+            self.error_queue,
+            max_pending_chunks=max_incoming_ports,
+            use_tls=use_tls,
+            use_compression=use_compression,
+            e2ee_key_bytes=e2ee_key_bytes,
+        )
+        self.gateway_sender = GatewaySender(
+            region,
+            self.chunk_store,
+            self.error_event,
+            self.error_queue,
+            outgoing_ports=outgoing_ports,
+            use_tls=use_tls,
+            use_compression=use_compression,
+            e2ee_key_bytes=e2ee_key_bytes,
+        )
+        provider = region.split(":")[0]
+        if provider == "aws" or provider == "gcp":
+            n_conn = 32
+        elif provider == "azure":
+            n_conn = 24  # due to throttling limits from authentication
+        self.obj_store_conn = GatewayObjStoreConn(self.chunk_store, self.error_event, self.error_queue, max_conn=n_conn)
+
+        # Download thread pool
+        self.dl_pool_semaphore = BoundedSemaphore(value=128)
+        self.ul_pool_semaphore = BoundedSemaphore(value=128)
+
+        # API server
+        self.api_server = GatewayDaemonAPI(self.chunk_store, self.gateway_receiver, self.error_event, self.error_queue)
+        self.api_server.start()
+        atexit.register(self.api_server.shutdown)
+        logger.info(f"[gateway_daemon] API started at {self.api_server.url}")
+
+    def run(self):
+        exit_flag = Event()
+
+        def exit_handler(signum, frame):
+            logger.warning("[gateway_daemon] Received signal {}. Exiting...".format(signum))
+            exit_flag.set()
+            self.gateway_receiver.stop_servers()
+            self.gateway_sender.stop_workers()
+            self.obj_store_conn.stop_workers()
+            sys.exit(0)
+
+        logger.info("[gateway_daemon] Starting gateway sender workers")
+        self.gateway_sender.start_workers()
+        self.obj_store_conn.start_workers()
+        signal.signal(signal.SIGINT, exit_handler)
+        signal.signal(signal.SIGTERM, exit_handler)
+
+        logger.info("[gateway_daemon] Starting daemon loop")
+        try:
+            while not exit_flag.is_set() and not self.error_event.is_set():
+                # queue object uploads and relays
+                for chunk_req in self.chunk_store.get_chunk_requests(ChunkState.downloaded):
+                    if self.region == chunk_req.dst_region and chunk_req.dst_type == "save_local":  # do nothing, save to ChunkStore
+                        self.chunk_store.state_queue_upload(chunk_req.chunk.chunk_id)
+                        self.chunk_store.state_start_upload(chunk_req.chunk.chunk_id, "save_local")
+                        self.chunk_store.state_finish_upload(chunk_req.chunk.chunk_id, "save_local")
+                        self.chunk_store.get_chunk_file_path(chunk_req.chunk.chunk_id).unlink()
+                    elif self.region == chunk_req.dst_region and chunk_req.dst_type == "object_store":
+                        self.chunk_store.state_queue_upload(chunk_req.chunk.chunk_id)
+                        self.obj_store_conn.queue_upload_request(chunk_req)
+                    elif self.region != chunk_req.dst_region:
+                        self.gateway_sender.queue_request(chunk_req)
+                        self.chunk_store.state_queue_upload(chunk_req.chunk.chunk_id)
+                    else:
+                        self.chunk_store.state_fail(chunk_req.chunk.chunk_id)
+                        self.error_queue.put(ValueError("[gateway_daemon] Unknown destination type"))
+                        self.error_event.set()
+
+                # queue object store downloads and relays (if space is available)
+                for chunk_req in self.chunk_store.get_chunk_requests(ChunkState.registered):
+                    if self.region == chunk_req.src_region and chunk_req.src_type == "read_local":  # do nothing, read from local ChunkStore
+                        self.chunk_store.state_queue_download(chunk_req.chunk.chunk_id)
+                        self.chunk_store.state_start_download(chunk_req.chunk.chunk_id, "read_local")
+                        self.chunk_store.state_finish_download(chunk_req.chunk.chunk_id, "read_local")
+                    elif self.region == chunk_req.src_region and chunk_req.src_type == "random":
+                        size_mb = chunk_req.src_random_size_mb
+                        assert chunk_req.src_random_size_mb is not None, "Random chunk size not set"
+                        if self.chunk_store.remaining_bytes() >= size_mb * MB * self.max_incoming_ports:
+
+                            def fn(chunk_req, size_mb):
+                                while self.chunk_store.remaining_bytes() < size_mb * MB * self.max_incoming_ports:
+                                    time.sleep(0.1)
+                                self.chunk_store.state_start_download(chunk_req.chunk.chunk_id, "random")
+                                fpath = str(self.chunk_store.get_chunk_file_path(chunk_req.chunk.chunk_id).absolute())
+                                with self.dl_pool_semaphore:
+                                    size_bytes = int(size_mb * MB)
+                                    assert size_bytes > 0, f"Invalid size {size_bytes} for fallocate"
+                                    os.system(f"fallocate -l {size_bytes} {fpath}")
+                                chunk_req.chunk.chunk_length_bytes = os.path.getsize(fpath)
+                                self.chunk_store.chunk_requests[chunk_req.chunk.chunk_id] = chunk_req
+                                self.chunk_store.state_finish_download(chunk_req.chunk.chunk_id, "random")
+
+                            self.chunk_store.state_queue_download(chunk_req.chunk.chunk_id)
+                            threading.Thread(target=fn, args=(chunk_req, size_mb)).start()
+                    elif self.region == chunk_req.src_region and chunk_req.src_type == "object_store":
+                        self.chunk_store.state_queue_download(chunk_req.chunk.chunk_id)
+                        self.obj_store_conn.queue_download_request(chunk_req)
+                    elif self.region != chunk_req.src_region:  # do nothing, waiting for chunk to be be ready_to_upload
+                        continue
+                    else:
+                        self.chunk_store.state_fail(chunk_req.chunk.chunk_id)
+                        self.error_queue.put(ValueError("[gateway_daemon] Unknown source type"))
+                        self.error_event.set()
+                time.sleep(0.1)  # yield
+        except Exception as e:
+            self.error_queue.put(e)
+            self.error_event.set()
+            logger.error(f"[gateway_daemon] Exception in daemon loop: {e}")
+            logger.exception(e)
+
+        # shut down workers except for API to report status
+        logger.info("[gateway_daemon] Exiting all workers except for API")
+        self.gateway_sender.stop_workers()
+        self.gateway_receiver.stop_servers()
+        self.obj_store_conn.stop_workers()
+        logger.info("[gateway_daemon] Done")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Skyplane Gateway Daemon")
+    parser.add_argument("--region", type=str, required=True, help="Region tag (provider:region")
+    parser.add_argument(
+        "--outgoing-ports", type=str, required=True, help="JSON encoded path mapping destination ip to number of outgoing ports"
+    )
+    parser.add_argument("--chunk-dir", type=Path, default="/tmp/skyplane/chunks", help="Directory to store chunks")
+    parser.add_argument("--disable-tls", action="store_true")
+    parser.add_argument("--use-compression", action="store_true")
+    parser.add_argument("--disable-e2ee", action="store_true")
+    args = parser.parse_args()
+
+    os.makedirs(args.chunk_dir)
+    daemon = GatewayDaemon(
+        region=args.region,
+        outgoing_ports=json.loads(args.outgoing_ports),
+        chunk_dir=args.chunk_dir,
+        use_tls=not args.disable_tls,
+        use_compression=args.use_compression,
+        use_e2ee=not args.disable_e2ee,
+    )
+    daemon.run()
---
+++ skyplane/gateway/gateway_obj_store.py
@@ -0,0 +1,171 @@
+import queue
+import time
+import traceback
+from dataclasses import dataclass
+from functools import partial
+from multiprocessing import Event, Manager, Process, Value, Queue
+
+from typing import Dict, Optional
+
+from skyplane.chunk import ChunkRequest
+from skyplane.config_paths import cloud_config
+from skyplane.gateway.chunk_store import ChunkStore
+from skyplane.obj_store.object_store_interface import ObjectStoreInterface
+from skyplane.utils import logger
+from skyplane.utils.retry import retry_backoff
+
+
+@dataclass
+class ObjStoreRequest:
+    chunk_req: ChunkRequest
+    req_type: str
+
+
+class GatewayObjStoreConn:
+    def __init__(self, chunk_store: ChunkStore, error_event, error_queue: Queue, max_conn=1):
+        self.chunk_store = chunk_store
+        self.error_event = error_event
+        self.error_queue = error_queue
+        self.n_processes = max_conn
+        self.processes = []
+        self.src_requester_pays = cloud_config.get_flag("requester_pays")
+
+        # shared state
+        self.manager = Manager()
+        self.next_worker_id = Value("i", 0)
+        self.worker_download_queue: queue.Queue[ObjStoreRequest] = self.manager.Queue()
+        self.worker_upload_queue: queue.Queue[ObjStoreRequest] = self.manager.Queue()
+        self.exit_flags = [Event() for _ in range(self.n_processes)]
+
+        # process-local state
+        self.worker_id: Optional[int] = None
+        self.obj_store_interfaces: Dict[str, ObjectStoreInterface] = {}
+
+    # interact with object store
+    def get_obj_store_interface(self, region: str, bucket: str) -> ObjectStoreInterface:
+        key = f"{region}:{bucket}"
+        if key not in self.obj_store_interfaces:
+            logger.warning(f"[gateway_daemon] ObjectStoreInterface not cached for {key}")
+            try:
+                self.obj_store_interfaces[key] = ObjectStoreInterface.create(region, bucket)
+            except Exception as e:
+                raise ValueError(f"Failed to create obj store interface {str(e)}")
+        return self.obj_store_interfaces[key]
+
+    def start_workers(self):
+        for i in range(self.n_processes):
+            p = Process(target=self.worker_loop, args=(i,))
+            p.start()
+            self.processes.append(p)
+
+    def stop_workers(self):
+        for i in range(self.n_processes):
+            self.exit_flags[i].set()
+        for p in self.processes:
+            p.join()
+        self.processes = []
+
+    def worker_loop(self, worker_id: int):
+        self.worker_id = worker_id
+        while not self.exit_flags[worker_id].is_set() and not self.error_event.is_set():
+            try:
+                # first try to get an upload request
+                try:
+                    request = self.worker_upload_queue.get_nowait()
+                    chunk_req = request.chunk_req
+                    req_type = request.req_type
+                except queue.Empty:
+                    # try to get a download request
+                    try:
+                        request = self.worker_download_queue.get_nowait()
+                        chunk_req = request.chunk_req
+                        req_type = request.req_type
+                    except queue.Empty:
+                        continue
+                fpath = str(self.chunk_store.get_chunk_file_path(chunk_req.chunk.chunk_id).absolute())
+
+                if req_type == "upload":
+                    assert chunk_req.dst_type == "object_store"
+                    bucket = chunk_req.dst_object_store_bucket
+                    self.chunk_store.state_start_upload(chunk_req.chunk.chunk_id, f"obj_store:{self.worker_id}")
+                    logger.debug(
+                        f"[obj_store:{self.worker_id}] Start upload {chunk_req.chunk.chunk_id} to {bucket}, key {chunk_req.chunk.dest_key}"
+                    )
+
+                    obj_store_interface = self.get_obj_store_interface(chunk_req.dst_region, bucket)
+                    retry_backoff(
+                        partial(
+                            obj_store_interface.upload_object,
+                            fpath,
+                            chunk_req.chunk.dest_key,
+                            chunk_req.chunk.part_number,
+                            chunk_req.chunk.upload_id,
+                            check_md5=chunk_req.chunk.md5_hash,
+                            mime_type=chunk_req.chunk.mime_type,
+                        ),
+                        max_retries=4,
+                    )
+                    chunk_file_path = self.chunk_store.get_chunk_file_path(chunk_req.chunk.chunk_id)
+                    self.chunk_store.state_finish_upload(chunk_req.chunk.chunk_id, f"obj_store:{self.worker_id}")
+                    chunk_file_path.unlink()
+                    logger.debug(f"[obj_store:{self.worker_id}] Uploaded {chunk_req.chunk.chunk_id} to {bucket}")
+                elif req_type == "download":
+                    assert chunk_req.src_type == "object_store"
+
+                    # wait for free space
+                    while self.chunk_store.remaining_bytes() < chunk_req.chunk.chunk_length_bytes * self.n_processes:
+                        time.sleep(0.1)
+
+                    bucket = chunk_req.src_object_store_bucket
+                    self.chunk_store.state_start_download(chunk_req.chunk.chunk_id, f"obj_store:{self.worker_id}")
+                    logger.debug(f"[obj_store:{self.worker_id}] Start download {chunk_req.chunk.chunk_id} from {bucket}")
+
+                    obj_store_interface = self.get_obj_store_interface(chunk_req.src_region, bucket)
+
+                    if self.src_requester_pays:
+                        obj_store_interface.set_requester_bool(True)
+                    mime_type, md5sum = retry_backoff(
+                        partial(
+                            obj_store_interface.download_object,
+                            chunk_req.chunk.src_key,
+                            fpath,
+                            chunk_req.chunk.file_offset_bytes,
+                            chunk_req.chunk.chunk_length_bytes,
+                            generate_md5=True,
+                        ),
+                        max_retries=4,
+                    )
+
+                    # update md5sum for chunk requests
+                    if not md5sum:
+                        logger.error(f"[obj_store:{self.worker_id}] Checksum was not generated for {chunk_req.chunk.src_key}")
+                    else:
+                        self.chunk_store.update_chunk_checksum(chunk_req.chunk.chunk_id, md5sum)
+                    if not mime_type:
+                        logger.error(f"[obj_store:{self.worker_id}] Mime type was not generated for {chunk_req.chunk.src_key}")
+                    else:
+                        self.chunk_store.update_chunk_mime_type(chunk_req.chunk.chunk_id, mime_type)
+
+                    self.chunk_store.state_finish_download(chunk_req.chunk.chunk_id, f"obj_store:{self.worker_id}")
+                    recieved_chunk_size = self.chunk_store.get_chunk_file_path(chunk_req.chunk.chunk_id).stat().st_size
+                    assert (
+                        recieved_chunk_size == chunk_req.chunk.chunk_length_bytes
+                    ), f"Downloaded chunk {chunk_req.chunk.chunk_id} to {fpath} has incorrect size (expected {chunk_req.chunk.chunk_length_bytes} but got {recieved_chunk_size}, {chunk_req.chunk.chunk_length_bytes})"
+                    logger.debug(
+                        f"[obj_store:{self.worker_id}] Downloaded {chunk_req.chunk.chunk_id} from {bucket} with {md5sum=} and {mime_type=}"
+                    )
+                else:
+                    raise ValueError(f"Invalid location for chunk req, {req_type}: {chunk_req.src_type}->{chunk_req.dst_type}")
+            except Exception as e:
+                logger.exception(f"[obj_store:{self.worker_id}] Exception in worker loop: {str(e)}")
+                self.error_queue.put(traceback.format_exc())
+                self.exit_flags[worker_id].set()
+                self.error_event.set()
+        # close destination sockets
+        logger.info(f"[obj_store:{worker_id}] exiting")
+
+    def queue_upload_request(self, chunk_request: ChunkRequest):
+        self.worker_upload_queue.put(ObjStoreRequest(chunk_request, "upload"))
+
+    def queue_download_request(self, chunk_request: ChunkRequest):
+        self.worker_download_queue.put(ObjStoreRequest(chunk_request, "download"))
