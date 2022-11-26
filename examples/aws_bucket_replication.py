903fb565e3f7020ca54616b1577e6cd83785149a
Sarah Wooders <sarahwooders@gmail.com>
2022-11-25 20:59:11 -0800

    cleanup

--- examples/aws_bucket_replication.py
+++ examples/aws_bucket_replication.py
@@ -13,7 +13,7 @@ from absl import flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string("src_region", None, "Source region")
 flags.DEFINE_spaceseplist("dst_regions", None, "Destination regions")
-flags.DEFINE_string("target_data", None, "Target data")
+flags.DEFINE_string("target_data", None, "Target data directory specified by S3 URI")
 
 def bucket_handle(region): 
     return f"broadcast-experiment-{region}"
@@ -21,15 +21,15 @@ def bucket_handle(region):
 def delete_policy(policy_arn):
     client = boto3.client("iam")
     response = client.delete_policy(PolicyArn=policy_arn)
-    print(response)
 
 def delete_role(role_name, policy_arn, batch_policy_arn):
     client = boto3.client("iam")
     response = client.detach_role_policy(RoleName=role_name, PolicyArn=policy_arn)
+    print("Successfully detached policy", policy_arn)
     response = client.detach_role_policy(RoleName=role_name, PolicyArn=batch_policy_arn)
-    print("Successfully detached policy", response)
+    print("Successfully detached policy", batch_policy_arn)
     response = client.delete_role(RoleName=role_name)
-    print(response)
+    print("Deleted role", role_name)
 
 def create_iam_role(src_region, dst_regions):
     client = boto3.client("iam")
@@ -118,13 +118,15 @@ def create_iam_role(src_region, dst_regions):
         PolicyDocument=json.dumps(policy)
     )
     policy_arn = response["Policy"]["Arn"]
+    print("Created policy ARN", policy_arn)
+
     response = client.create_policy(
         PolicyName="batch"+policy_name, 
         PolicyDocument=json.dumps(batch_policy)
     )
     batch_policy_arn = response["Policy"]["Arn"]
+    print("Created batch policy ARN", batch_policy_arn)
  
-    print(response)
 
     assume_role_policy_document = {
         "Version": "2012-10-17",
@@ -151,19 +153,15 @@ def create_iam_role(src_region, dst_regions):
         AssumeRolePolicyDocument=json.dumps(assume_role_policy_document)
     )
     role_arn = resp["Role"]["Arn"]
-    print(resp)
+    print("Created role", role_name, role_arn)
 
     time.sleep(5)
     response = client.attach_role_policy(RoleName=role_name, PolicyArn=policy_arn)
     response = client.attach_role_policy(RoleName=role_name, PolicyArn=batch_policy_arn)
-    print(response)
-    
 
     response = client.get_role(
         RoleName=role_name
     )
-    print("get role", response)
-    print("ARN", role_arn)
 
     return role_name, role_arn, policy_arn, batch_policy_arn
 
@@ -173,59 +171,44 @@ def write_source_data(src_region, target_data, directory):
     print("Syncing data to source bucket", sync_command)
     os.system(sync_command)
 
-def main(argv):
 
-    auth = AWSAuthentication()
+def main(argv):
 
     src_region = FLAGS.src_region
     dst_regions = list(FLAGS.dst_regions)
-    print("Destinations:", dst_regions)
     experiment_name = f"aws_replication_{int(time.time())}"
+    print("Destinations:", dst_regions)
     
     buckets = {}
 
     # create temporary bucket for each region 
     for region in [src_region] + dst_regions: 
         region = region.split(":")[1]
-        print("REGION", region)
         bucket_name = bucket_handle(region)
-        client = auth.get_boto3_client("s3", region)
+        bucket = S3Interface(bucket_name)
 
-        try:
-            client.list_objects_v2(Bucket=bucket_name, MaxKeys=1)
-            print(f"Bucket {bucket_name} already exists")
-        except Exception as e:
+        if not bucket.bucket_exists():
             print(f"Bucket {bucket_name} does not exist, creating it")
+            bucket.create_bucket()
+            print(f"Created bucket {bucket_name} in {region}")
 
-            if region == "us-east-1":
-                bucket = client.create_bucket(Bucket=bucket_name)
-            else:
-                bucket = client.create_bucket(
-                    Bucket=bucket_name, 
-                    CreateBucketConfiguration={"LocationConstraint": region}
-                )
-            print(f"created bucket {bucket_name} in {region}")
-
-        buckets[region] = S3Interface(bucket_name) 
-        response = client.put_bucket_versioning(
+        buckets[region] = bucket
+        response = bucket._s3_client(region).put_bucket_versioning(
             Bucket=bucket_name,
             VersioningConfiguration={
                 'Status': 'Enabled'
             }
         )
-        print("Enabled bucket versioning", response)
+        print(f"Enabled bucket versioning for {bucket_name}")
 
 
-    print()
-    print() 
-    print(dst_regions)
     # put replication policy 
     src_name = bucket_handle(src_region.split(":")[1])
-    # creat iam role
+    # creat iam roles
     role_name, role_arn, policy_arn, batch_policy_arn = create_iam_role(src_region, dst_regions)
     time.sleep(5)
 
-    client = auth.get_boto3_client("s3", src_region.split(":")[1])
+    client = buckets[src_region.split(":")[1]]._s3_client(src_region.split(":")[1])
     rules = []
     for dst_region in dst_regions:
         dest_name = bucket_handle(dst_region.split(":")[1])
@@ -268,48 +251,56 @@ def main(argv):
                 'Rules': rules
             }
         )
-        print(resp)
     except Exception as e:
         delete_role(role_name, policy_arn)
         delete_policy(policy_arn)
+        delete_policy(batch_policy_arn)
         print("Error creating replication rule", e) 
         return 
 
-    # write data to source
+    # write data to source - use skyplane to quickly copy some target data into the source bucket 
+    # NOTE: only data copied after the replication rules are created will get copied
     src_region = src_region.split(":")[1]
+    target_data_bucket = FLAGS.target_data.split("/")[2]
+    target_data_region = S3Interface(target_data_bucket).aws_region
     src_bucket = bucket_handle(src_region)
-    # TODO: replace with Skyplane
     client = skyplane.SkyplaneClient(aws_config=skyplane.AWSConfig())
     print(f"Log dir: {client.log_dir}/client.log")
-    dp = client.dataplane("aws", "us-east-1", "aws", "us-east-1", n_vms=4)
+
+    dp = client.dataplane("aws", target_data_region, "aws", src_region, n_vms=4) # TODO: pass in target_throughput that we also pass to broadcast?
     with dp.auto_deprovision():
+
+        # copy data with skyplane to source bucket
         dp.provision(spinner=True)
         dp.queue_copy(
             FLAGS.target_data, f"s3://{src_bucket}/{experiment_name}", recursive=True
         )
-        tracker = dp.run()
-        #write_source_data(src_region, FLAGS.target_data, experiment_name)
+        print("Waiting for data to copy to source bucket...")
+        dp.run()
 
         # wait for copy at destinations
         target_objects = list(buckets[src_region].list_objects(prefix=experiment_name))
         print(f"Waiting for len(target_objects) to replicate", experiment_name)
         num_src = len(target_objects)
+        # TODO: replace with better monitoring
         while True: 
             completed = 0
             for region, bucket in buckets.items(): 
+                if region == src_region: continue 
                 objs = list(bucket.list_objects(prefix=experiment_name))
                 num_dest = len(objs)
                 print(f"{region}: Object replicated = {len(objs)} / {len(target_objects)}")
                 if num_dest == num_src: 
                     completed += 1
 
-            if completed == len(list(buckets.keys())):
+            if completed == len(list(buckets.keys())) - 1:
                 print("All replication completed!")
                 break 
 
 
-            time.sleep(10)
+            time.sleep(1)
 
+    # cleanup IAM roles and policies 
     delete_role(role_name, policy_arn, batch_policy_arn)
     delete_policy(policy_arn)
     delete_policy(batch_policy_arn)
