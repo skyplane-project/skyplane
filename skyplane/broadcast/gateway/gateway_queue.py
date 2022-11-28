99453d0791136ff3fb3f8e63db6cd491e58297e6
Sarah Wooders <sarahwooders@gmail.com>
2022-11-27 17:16:02 -0800

    add gateway files

---
+++ skyplane/broadcast/gateway/gateway_queue.py
@@ -0,0 +1,51 @@
+from multiprocessing import Queue, Manager
+import copy
+
+
+class GatewayQueue:
+    def __init__(self, maxsize=0):
+        self.q = Queue(maxsize)
+        self.handles = []
+
+    def register_handle(self, requester_handle):
+        self.handles.append(requester_handle)
+
+    def put(self, chunk_req):
+        self.q.put(chunk_req)
+
+    def pop(self, requester_handle=None):
+        self.q.pop()
+
+    def get_nowait(self, requester_handle=None):
+        return self.q.get_nowait()
+
+    def get_handles(self):
+        return self.handles
+
+
+class GatewayANDQueue(GatewayQueue):
+    def __init__(self, maxsize=0):
+        self.q = {}
+        self.maxsize = maxsize
+
+    def register_handle(self, requester_handle):
+        # create a queue per handle (operator)
+        self.q[requester_handle] = GatewayQueue(self.maxsize)
+
+    def get_handles(self):
+        return list(self.q.keys())
+
+    def get_handle_queue(self, requester_handle):
+        return self.q[requester_handle]
+
+    def put(self, chunk_req):
+        # place chunk in all downstream operators queues
+        for handle in self.q.keys():
+            # self.q[handle].put(copy.deepcopy(chunk_req))
+            self.q[handle].put(chunk_req)
+
+    def pop(self, requester_handle):
+        self.q[requester_handle].pop()
+
+    def get_nowait(self, requester_handle):
+        return self.q[requester_handle].get_nowait()
--- skyplane/broadcast/test/bc_demo.py
+++ skyplane/broadcast/test/bc_demo.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     print(f"Log dir: {client.log_dir}/client.log")
     dp = client.broadcast_dataplane(
         src_cloud_provider="aws",
-        src_region="us-east-1",
+        src_region="ap-northeast-2",
         dst_cloud_providers=["aws", "aws"],
-        dst_regions=["us-west-1", "us-west-2"],
+        dst_regions=["ap-south-1", "us-east-1"],
         n_vms=1,
         # gbyte_to_transfer=32 NOTE: might need to fix the calculation of topo.cost_per_gb until real data is passed
     )
@@ -25,11 +25,17 @@ if __name__ == "__main__":
     with dp.auto_deprovision():
         # NOTE: need to queue copy first, then provision
         # NOTE: otherwise can't upload gateway programs to the gateways, don't know the bucket name and object name
+
+        source_file = "s3://sarah-skylark-ap-northeast-2/medium_file.txt"
+        dest1_file = "s3://broadcast-experiment-ap-south-1/medium_file.txt"
+        dest2_file = "s3://broadcast-experiment-us-east-1/medium_file.txt"
+
+
         dp.queue_copy(
-            "s3://awsbucketsky/chunk_0",
+            source_file, 
             [
-                "s3://awsbucketsky2/chunk_0",
-                "s3://awsbucketsky3/chunk_0",
+                dest1_file, 
+                dest2_file
             ],
             recursive=False,
         )
