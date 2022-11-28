0da94beee7a5e4c3fe8d3e66a0ddd375259c3f0e
Shu Liu <sliu583@wisc.edu>
2022-11-28 00:03:56 -0800

    fix tracker and type of chunk_id

--- skyplane/broadcast/test/bc_demo.py
+++ skyplane/broadcast/test/bc_demo.py
@@ -8,11 +8,9 @@ if __name__ == "__main__":
     print(f"Log dir: {client.log_dir}/client.log")
     dp = client.broadcast_dataplane(
         src_cloud_provider="aws",
-        #src_region="us-east-1",
-        src_region="ap-northeast-2", 
+        src_region="us-east-1",
         dst_cloud_providers=["aws", "aws"],
-        #dst_regions=["us-west-1", "us-west-2"],
-        dst_regions=["ap-south-1", "us-east-1"], 
+        dst_regions=["us-west-1", "us-west-2"],
         n_vms=1,
         # gbyte_to_transfer=32 NOTE: might need to fix the calculation of topo.cost_per_gb until real data is passed
     )
@@ -21,13 +19,9 @@ if __name__ == "__main__":
         # NOTE: need to queue copy first, then provision
         # NOTE: otherwise can't upload gateway programs to the gateways, don't know the bucket name and object name
 
-        #source_file = "s3://awsbucketsky/chunk_0"
-        #dest1_file = "s3://awsbucketsky2/chunk_0"
-        #dest2_file = "s3://awsbucketsky3/chunk_0"
-
-        source_file = "s3://sarah-skylark-ap-northeast-2/chunk_2"
-        dest1_file = "s3://broadcast-experiment-ap-south-1/chunk_2"
-        dest2_file = "s3://broadcast-experiment-us-east-1/chunk_2"
+        source_file = "s3://awsbucketsky/chunk_0"
+        dest1_file = "s3://awsbucketsky2/chunk_0"
+        dest2_file = "s3://awsbucketsky3/chunk_0"
 
         dp.queue_copy(
             source_file,
@@ -52,7 +46,7 @@ if __name__ == "__main__":
             if bytes_remaining is None:
                 print(f"{timestamp} Transfer not yet started")
             elif bytes_remaining > 0:
-                print(f"{timestamp} {(bytes_remaining / (2 ** 30)):.2f}GB left")
+                print(f"{timestamp} {(bytes_remaining / (2 ** 30)):.5f}GB left")
             else:
                 break
             time.sleep(1)
