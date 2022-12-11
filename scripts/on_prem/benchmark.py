c8a33f2fb5cdf9bdc0708d429acaef0537c23ed8
Hailey Jang <hailey.jang.01@gmail.com>
2022-12-11 13:48:07 -0800

    Implement multiprocessing to benchmark

--- scripts/on_prem/benchmark.py
+++ scripts/on_prem/benchmark.py
@@ -2,16 +2,17 @@ import os
 import time
 from pyarrow import fs
 import argparse
-import threading
-from multiprocessing.pool import ThreadPool
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 KB = 1024
 MB = 1024 * 1024
 GB = MB * 1024
 
-THREADS = 8
+THREADS = 32
 
-def transfer_file(in_fs, in_path, out_fs, out_path, BATCH_SIZE, waiting, sema, start, final):
+def transfer_file(in_fs, in_path, out_fs, out_path, BATCH_SIZE, final):
+    print("Starting transfer...")
     if out_fs is not None:
         with in_fs.open_input_stream(in_path) as in_file:
             with out_fs.open_output_stream(out_path) as out_file:
@@ -22,19 +23,14 @@ def transfer_file(in_fs, in_path, out_fs, out_path, BATCH_SIZE, waiting, sema, s
                     else:
                         break
     else:
-        with in_fs.open_input_stream(in_path) as in_file:
-            in_file.seek(start)
-            while in_file.tell() < start+final:
-                buf = in_file.read(BATCH_SIZE)
-                #print(f"Reading!{threading.get_ident()}", flush=True)
-                if not buf:
-                    if waiting._Semaphore__value == THREADS:
-                        sema.release(THREADS)
-                        return
-                    else:
-                        waiting.release()
-                        sema.acquire()
-                    break
+        in_file = in_fs.open_input_stream(in_path)
+        curr = 0
+        while curr < final:
+            buf = in_file.read(BATCH_SIZE)
+            #print(f"Reading!{threading.get_ident()}", flush=True)
+            curr += BATCH_SIZE
+            if not buf:
+                break
 
 
 def setup_files_and_dirs(outdir, hdfs):
@@ -74,42 +70,46 @@ def transfer_hdfs_to_local(hdfs, local, outdir):
 
 
 def parallel_reads(args):
-    (hdfs, lock, sema, start, final) = args
-    # new_hdfs = fs.HadoopFileSystem(host=hdfs, port=8020, extra_conf={"dfs.client.use.datanode.hostname": "false"})
-    transfer_file(hdfs, f"/data/10GBdata.bin", None, f"data/10GBdata.bin", 128 * MB, lock, sema, start, final)
-
+    (hdfs, final) = args
+    new_hdfs = fs.HadoopFileSystem(host=hdfs, port=8020, extra_conf={"dfs.client.use.datanode.hostname": "false"})
+    transfer_file(new_hdfs, f"/data/10GBdata.bin", None, f"data/10GBdata.bin", 128 * MB, final)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("HDFS", type=str, help="HDFS host")
     parser.add_argument("--outdir", type=str, default="/tmp/data")
+    parser.add_argument("--threads", type=bool, default=True)
     args = parser.parse_args()
 
     hdfs = fs.HadoopFileSystem(host=args.HDFS, port=8020, user="hadoop", extra_conf={"dfs.client.use.datanode.hostname": "false"})
     local = fs.LocalFileSystem()
+    thread = args.threads
     #setup_files_and_dirs(args.outdir, hdfs)
     #transfer_local_to_hdfs(hdfs, local, args.outdir)
-    # transfer_hdfs_to_local(hdfs, local)
-    sema = threading.Semaphore(value=0)
-    lock = threading.Lock()
-    #t1 = threading.Thread(target=parallel_reads, args=(hdfs, lock, sema))
-    args = []
+    #transfer_hdfs_to_local(hdfs, local)
+    arg = []
     increment = 10*GB/THREADS
-    waiting = threading.Semaphore(value=0)
     curr = 0
-    
+    multiprocessing.set_start_method("spawn")
     #prepare args
     for i in range(THREADS):
-        args.append((hdfs, sema, waiting, curr, increment))
+        arg.append((args.HDFS, increment))
         curr += increment
-        
+    if (thread):
     #execute the threads
-    with ThreadPool(THREADS) as p:
-        before = time.time()
-        p.map_async(parallel_reads, args)
-    sema.acquire()
+        with ThreadPoolExecutor(max_workers=THREADS) as p:
+            before = time.time()
+            future = [p.submit(parallel_reads, arg[i]) for i in range(THREADS)]
+            #p.map(parallel_reads, args)
+    else:
+        with ProcessPoolExecutor(max_workers=THREADS) as p:
+            before = time.time()
+            future = [p.submit(parallel_reads, arg[i]) for i in range(THREADS)]
+            #print(future.result())
+            #p.map(example, args)
 
-    print(f"Finished!Time:{time.time()-before}")
+
+    print(f"Finished! Time:{time.time()-before}")
     """
     t1 = threading.Thread(target=parallel_reads, args=(hdfs, lock, sema))
     t2 = threading.Thread(target=parallel_reads, args=(hdfs, lock, sema))
