1dd8ab41c8817123c3011df8cb0837867c55a920
Hailey Jang <hailey.jang.01@gmail.com>
2023-02-06 09:32:45 -0800

    Created draft dataproc test

---
+++ tests/unit_gcs/test_dataproc.py
@@ -0,0 +1,54 @@
+import uuid
+from skyplane.config_paths import cloud_config
+from skyplane.obj_store.object_store_interface import ObjectStoreInterface
+from tests.interface_util import interface_test_framework
+from skyplane.utils import imports
+from google.cloud import dataproc_v1 as dataproc
+
+def test_dataproc():
+
+    cluster_name = "skyplane-dataproc-test"
+    region = "us-central1"
+    project_id ="skyplane-hailey-373919"
+
+    
+    # Create a client with the endpoint set to the desired cluster region.
+    cluster_client = dataproc.ClusterControllerClient(
+        client_options={"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
+    )
+
+    # Create the cluster config.
+    cluster = {
+        "project_id": project_id,
+        "cluster_name": cluster_name,
+        "config": {
+            "master_config": {"num_instances": 1, "machine_type_uri": "n1-standard-2"},
+            "worker_config": {"num_instances": 2, "machine_type_uri": "n1-standard-2"},
+        },
+    }
+
+    # Create the cluster.
+    operation = cluster_client.create_cluster(
+        request={"project_id": project_id, "region": region, "cluster": cluster}
+    )
+    result = operation.result()
+
+    # Output a success message.
+    print(f"Cluster created successfully: {result.cluster_name}")
+    # [END dataproc_create_cluster]
+
+    print("Cluster created successfully: {}".format(result.cluster_name))
+    
+    #assert interface_test_framework("azure:eastus", bucket_name, False, test_delete_bucket=True)
+    
+        # Delete the cluster once the job has terminated.
+    operation = cluster_client.delete_cluster(
+        request={
+            "project_id": project_id,
+            "region": region,
+            "cluster_name": cluster_name,
+        }
+    )
+    operation.result()
+
+    print("Cluster {} successfully deleted.".format(cluster_name))
