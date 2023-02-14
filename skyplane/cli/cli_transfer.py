b5cfb196b99d2a158058c9bf104be27c3d568a1c
Asim Biswal <abiswal@berkeley.edu>
2023-02-14 19:37:06 +0000

    adding import

--- skyplane/cli/cli_transfer.py
+++ skyplane/cli/cli_transfer.py
@@ -26,6 +26,7 @@ from skyplane.obj_store.object_store_interface import ObjectStoreInterface
 from skyplane.cli.impl.progress_bar import ProgressBarTransferHook
 from skyplane.utils import logger
 from skyplane.utils.definitions import GB, format_bytes
+from skyplane.utils.fn import do_parallel
 from skyplane.utils.path import parse_path
 
 
