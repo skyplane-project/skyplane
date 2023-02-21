f85d79271ac290ce6e51dabfe76e402f660090f9
Sarah Wooders <sarahwooders@gmail.com>
2023-02-20 16:22:13 -0800

    more cleanup

--- skyplane/cli/cli_init.py
+++ skyplane/cli/cli_init.py
@@ -322,8 +322,6 @@ def load_gcp_config(config: SkyplaneConfig, force_init: bool = False, non_intera
 
         # check if GCP is enabled
         inferred_cred, inferred_project = compute.GCPAuthentication.get_adc_credential()
-        print("inferred_cred", inferred_cred)
-        print("infreferred_project", inferred_project)
         if inferred_cred is None:  # or inferred_project is None:
             typer.secho("    Default GCP credentials are not set up yet. Run `gcloud auth application-default login`.", fg="red", err=True)
             typer.secho("    https://cloud.google.com/docs/authentication/getting-started", fg="red", err=True)
