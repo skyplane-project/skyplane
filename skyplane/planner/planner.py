e0d906d2b9d6844206bbac79cc4a3774d45db64a
xTRam1 <lerdogan@berkeley.edu>
2023-05-15 15:38:06 -0700

    better logic

--- skyplane/planner/planner.py
+++ skyplane/planner/planner.py
@@ -49,28 +49,28 @@ class Planner:
         vm_family = "Standard_D{}_v5" if cloud_provider == "azure" else "n2-standard-{}"
         return vm_family.format(vcpus)
 
-    @staticmethod
-    def _split_vcpus(num_vcpus: int, quota_limit: int) -> Optional[Tuple]:
-        """Splits the total/target number of vcpus used into smaller partitions according to the quota limit with the
-        given vcpu options. These options are the same for all providers for now. Enforces that the vcpu_count is
-        all the same for all partitions, meaning that for a num_vcpus=32, you cannot partition into 1 vm of 16 vcpus and
-        2 vms of 8 vcpus but can partition into 4 vms of 8 vcpus. Returns the number of partitions and their vcpus usage
-        """
-        # If the desired vCPU count is within the quota limit, don't fall back
-        if num_vcpus <= quota_limit:
-            return (1, num_vcpus)
+    # @staticmethod
+    # def _split_vcpus(num_vcpus: int, quota_limit: int) -> Optional[Tuple]:
+    #     """Splits the total/target number of vcpus used into smaller partitions according to the quota limit with the
+    #     given vcpu options. These options are the same for all providers for now. Enforces that the vcpu_count is
+    #     all the same for all partitions, meaning that for a num_vcpus=32, you cannot partition into 1 vm of 16 vcpus and
+    #     2 vms of 8 vcpus but can partition into 4 vms of 8 vcpus. Returns the number of partitions and their vcpus usage
+    #     """
+    #     # If the desired vCPU count is within the quota limit, don't fall back
+    #     if num_vcpus <= quota_limit:
+    #         return (1, num_vcpus)
 
-        # Otherwise, try to split the desired vCPU count into smaller portions that are within the quota limit and use the largest option
-        else:
-            for vcpu_count in Planner._VCPUS:
-                if vcpu_count <= quota_limit and vcpu_count <= num_vcpus:
-                    portions = num_vcpus // vcpu_count
-                    remaining_vcpus = num_vcpus - (vcpu_count * portions)
-                    # If the remaining vCPUs are 0, use the current option to launch all portions
-                    if remaining_vcpus == 0:
-                        return (portions, vcpu_count)  # [vcpu_count] * portions
-        # Return None if no valid vCPU portions were found
-        return None
+    #     # Otherwise, try to split the desired vCPU count into smaller portions that are within the quota limit and use the largest option
+    #     else:
+    #         for vcpu_count in Planner._VCPUS:
+    #             if vcpu_count <= quota_limit and vcpu_count <= num_vcpus:
+    #                 portions = num_vcpus // vcpu_count
+    #                 remaining_vcpus = num_vcpus - (vcpu_count * portions)
+    #                 # If the remaining vCPUs are 0, use the current option to launch all portions
+    #                 if remaining_vcpus == 0:
+    #                     return (portions, vcpu_count)  # [vcpu_count] * portions
+    #     # Return None if no valid vCPU portions were found
+    #     return None
 
 
 class UnicastDirectPlanner(Planner):
@@ -171,27 +171,17 @@ class MulticastDirectPlanner(Planner):
 
         plan = TopologyPlan(src_region_tag=src_region_tag, dest_region_tags=dst_region_tags)
 
-        # Dynammically calculate n_instances based on quota limits - do_parallel returns tuples of (target_vcpus, quota_limit)
-        targets = do_parallel(self._calculate_vm_portions, [(src_region_tag, dst_region_tag) for dst_region_tag in dst_region_tags])
-        targets = [t[1] for t in targets]
-        min_target_vcpus = min(t[0] for t in targets)  # type: ignore
-        min_quota_limit = min(t[1] for t in targets if t[1] is not None)  # type: ignore
-        n_portions, vcpus = (
-            Planner._split_vcpus(num_vcpus=min_target_vcpus, quota_limit=min_quota_limit)
-            if min_quota_limit is not None
-            else (1, min_target_vcpus)
-        )  # type: ignore
-
-        # Get the source vm_type
-        src_provider = src_region_tag.split(":")[0]
-        src_vm_type = Planner._vcpus_to_vm(cloud_provider=src_provider, vcpus=vcpus)
+        # Dynammically calculate n_instances based on quota limits - do_parallel returns
+        # tuples of (vcpus, n_instances)
+        vm_info = do_parallel(self._calculate_vm_types, [src_region_tag] + dst_region_tags)  # type: ignore
+        vm_types = {v[0]: Planner._vcpus_to_vm(cloud_provider=v[0].split(":")[0], vcpus=v[1][0]) for v in vm_info}  # type: ignore
+        n_instances = min(v[1][1] for v in vm_info)  # type: ignore
 
         # TODO: support on-sided transfers but not requiring VMs to be created in source/destination regions
-        for i in range(n_portions * self.n_instances):
-            plan.add_gateway(src_region_tag, src_vm_type)
+        for i in range(n_instances):
+            plan.add_gateway(src_region_tag, vm_types[src_region_tag])
             for dst_region_tag in dst_region_tags:
-                dst_provider = dst_region_tag.split(":")[0]
-                plan.add_gateway(dst_region_tag, Planner._vcpus_to_vm(cloud_provider=dst_provider, vcpus=vcpus))
+                plan.add_gateway(dst_region_tag, vm_types[dst_region_tag])
 
         # initialize gateway programs per region
         dst_program = {dst_region: GatewayProgram() for dst_region in dst_region_tags}
@@ -231,7 +221,7 @@ class MulticastDirectPlanner(Planner):
 
                 # can send to any gateway in region
                 mux_or = src_program.add_operator(GatewayMuxOr(), parent_handle=mux_and, partition_id=partition_id)
-                for i in range(n_portions * self.n_instances):
+                for i in range(n_instances):
                     private_ip = False
                     if dst_gateways[i].provider == "gcp" and src_provider == "gcp":
                         # print("Using private IP for GCP to GCP transfer", src_region_tag, dst_region_tag)
@@ -287,40 +277,48 @@ class MulticastDirectPlanner(Planner):
                     return quota["spot_standard_vcpus"] if spot else quota["on_demand_standard_vcpus"]
         return None
 
-    def _calculate_vm_portions(self, tags: Tuple[str, str]) -> Tuple[int, Optional[int]]:
-        """Calculates how to split the source and destination vms according to the quota limits on number of vcpus.
-        Constraints:
-            - Take the minimum of the quota limits of source and destination
-            - If the configured vm types for source and destination don't launch the same number of vcpus, take
-            the min of them
-        As a result, both the source and the destination use the same number of partitions with the same number of vcpus
-        on each partition. Returns the number of partitions and vcpus.
+    def _calculate_vm_types(self, region_tag: str) -> Optional[Tuple[int, int]]:
+        """Calculates the largest allowed vm type according to the regional quota limit as well as
+        how many of these vm types can we launch to avoid QUOTA_EXCEEDED errors. Returns:
+            - vcpus: Number of vcpus used by the selected vm type
+            - n_instances: Number of instance allowed by the quota limit
+        Returns None if quota information wasn't properly loaded or allowed vcpu list is wrong.
         """
-        src_tag, destination_tag = tags
+        cloud_provider, region = region_tag.split(":")
 
-        # Get source vcpu info
-        src_provider, src_region = src_tag.split(":")
-        src_vm_type = getattr(self.transfer_config, f"{src_provider}_instance_class")
-        src_target_vcpus = Planner._vm_to_vcpus(src_provider, src_vm_type)
-
-        # Get destination vcpu info
-        dst_provider, dst_region = destination_tag.split(":")
-        dst_vm_type = getattr(self.transfer_config, f"{dst_provider}_instance_class")
-        dst_target_vcpus = Planner._vm_to_vcpus(dst_provider, dst_vm_type)
-
-        # Get the quota info for both
-        src_quota_limit = self._get_quota_limits_for(
-            cloud_provider=src_provider, region=src_region, spot=getattr(self.transfer_config, f"{src_provider}_use_spot_instances")
+        # Get the quota limit
+        quota_limit = self._get_quota_limits_for(
+            cloud_provider=cloud_provider, region=region, spot=getattr(self.transfer_config, f"{cloud_provider}_use_spot_instances")
         )
-        dst_quota_limit = self._get_quota_limits_for(
-            cloud_provider=dst_provider, region=dst_region, spot=getattr(self.transfer_config, f"{dst_provider}_use_spot_instances")
-        )
-        quota_limit = min(src_quota_limit, dst_quota_limit) if src_quota_limit is not None and dst_quota_limit is not None else None
 
-        # If the target vcpus are not the same, also take the min of target vcpus
-        target_vcpus = src_target_vcpus if src_target_vcpus == dst_target_vcpus else min(src_target_vcpus, dst_target_vcpus)
+        # No quota limits (quota limits weren't initialized properly during skyplane init)
+        if quota_limit is None:
+            return None
 
-        return target_vcpus, quota_limit
+        config_vm_type = getattr(self.transfer_config, f"{cloud_provider}_instance_class")
+        config_vcpus = Planner._vm_to_vcpus(cloud_provider, config_vm_type)
+        if config_vcpus <= quota_limit:
+            return config_vcpus, 1  # TODO: read this 1 from the config file
+
+        # Perform binary search to find the largest vcpu count that is less than the quota limit
+        left = 0
+        right = len(Planner._VCPUS) - 1
+        vcpus = None
+        while left <= right:
+            mid = (left + right) // 2
+            if Planner._VCPUS[mid] <= quota_limit:
+                vcpus = Planner._VCPUS[mid]
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        # shouldn't happen, but just in case we use more complicated vm types in the future
+        if vcpus is None:
+            return None
+
+        # number of instances allowed by the quota with the selected vm type
+        n_instances = quota_limit // vcpus
+        return (vcpus, n_instances)
 
 
 class UnicastILPPlanner(Planner):
--- tests/test_fall_back.py
+++ tests/test_fall_back.py
@@ -22,38 +22,19 @@ def test_fall_back():
     planner = MulticastDirectPlanner(n_instances=8, n_connections=100, transfer_config=transfer_config)
     planner.quota_limits = quota_limits
 
-    # aws: 5
-    # azure: 4
-    # gcp: 8
     region_tags = ["aws:us-east-1", "azure:uaenorth", "gcp:us-east1"]
-    tests: Dict[Tuple[str, str], Tuple[int, int, str]] = {
-        ("aws:us-east-1", "aws:us-east-1"): (8, 4, "m5.xlarge"),
-        ("aws:us-east-1", "azure:uaenorth"): (1, 2, "m5.large"),
-        ("aws:us-east-1", "gcp:us-east1"): (4, 4, "m5.xlarge"),
-        ("azure:uaenorth", "aws:us-east-1"): (1, 2, "Standard_D2_v5"),
-        ("azure:uaenorth", "azure:uaenorth"): (1, 2, "Standard_D2_v5"),
-        ("azure:uaenorth", "gcp:us-east1"): (1, 2, "Standard_D2_v5"),
-        ("gcp:us-east1", "aws:us-east-1"): (4, 4, "n2-standard-4"),
-        ("gcp:us-east1", "azure:uaenorth"): (1, 2, "n2-standard-2"),
-        ("gcp:us-east1", "gcp:us-east1"): (2, 8, "n2-standard-8"),
+    test_vm_types = {"aws:us-east-1": "m5.xlarge", "azure:uaenorth": "Standard_D4_v5", "gcp:us-east1": "n2-standard-8"}
+    test_n_instances = {
+        "aws:us-east-1": 1,
+        "azure:uaenorth": 1,
+        "gcp:us-east1": 1,
     }
 
-    for src_region_tag in region_tags:
-        for dst_region_tags in region_tags:
-            targets = do_parallel(
-                planner._calculate_vm_portions, [(src_region_tag, dst_region_tag) for dst_region_tag in [dst_region_tags]]
-            )
-            targets = [t[1] for t in targets]
-            min_target_vcpus = min(t[0] for t in targets)  # type: ignore
-            min_quota_limit = min(t[1] for t in targets if t[1] is not None)  # type: ignore
-            n_portions, vcpus = (
-                Planner._split_vcpus(num_vcpus=min_target_vcpus, quota_limit=min_quota_limit)
-                if min_quota_limit is not None
-                else (1, min_target_vcpus)
-            )  # type: ignore
+    for i, src_region_tag in enumerate(region_tags):
+        dst_region_tags = region_tags[:i] + region_tags[i + 1 :]
+        vm_info = do_parallel(planner._calculate_vm_types, [src_region_tag] + dst_region_tags)  # type: ignore
+        vm_types = {v[0]: Planner._vcpus_to_vm(cloud_provider=v[0].split(":")[0], vcpus=v[1][0]) for v in vm_info}  # type: ignore
+        n_instances = min(v[1][1] for v in vm_info)  # type: ignore
 
-            # Get the source vm_type
-            src_provider = src_region_tag.split(":")[0]
-            src_vm_type = Planner._vcpus_to_vm(cloud_provider=src_provider, vcpus=vcpus)
-
-            assert tests[(src_region_tag, dst_region_tags)] == (n_portions, vcpus, src_vm_type)
+        assert vm_types == test_vm_types, "vm types are calculated wrong"
+        assert test_n_instances[src_region_tag] == n_instances
