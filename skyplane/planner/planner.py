00e1624d423b10f511e2ce7246885918bd7eba17
xTRam1 <lerdogan@berkeley.edu>
2023-06-06 18:56:15 -0700

    little changes

--- skyplane/planner/planner.py
+++ skyplane/planner/planner.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 from importlib.resources import path
 from typing import Dict, List, Optional, Tuple, Tuple
-import re
 import os
+import csv
 
 from skyplane import compute
 from skyplane.api.config import TransferConfig
@@ -27,9 +28,6 @@ from skyplane.config import SkyplaneConfig
 
 
 class Planner:
-    # Only supporting "aws:m5.", "azure:StandardD_v5", and "gcp:n2-standard" instances for now
-    _VCPUS = (96, 64, 48, 32, 16, 8, 4, 2)
-
     def __init__(self, transfer_config: TransferConfig):
         self.transfer_config = transfer_config
         self.config = SkyplaneConfig.load_config(config_path)
@@ -46,49 +44,31 @@ class Planner:
             with gcp_quota_path.open("r") as f:
                 self.quota_limits["gcp"] = json.load(f)
 
+        # Loading the vcpu information - a dictionary of dictionaries
+        # {"cloud_provider": {"instance_name": vcpu_cost}}
+        self.vcpu_info = defaultdict(dict)
+        with path("skyplane.data", "vcpu_info.csv") as file_path:
+            with open(file_path, "r") as csvfile:
+                reader = csv.reader(csvfile)
+                next(reader)  # Skip the header row
+
+                for row in reader:
+                    instance_name, cloud_provider, vcpu_cost = row
+                    vcpu_cost = int(vcpu_cost)
+                    self.vcpu_info[cloud_provider][instance_name] = vcpu_cost
+
     def plan(self) -> TopologyPlan:
         raise NotImplementedError
 
-    @staticmethod
-    def _vm_to_vcpus(cloud_provider: str, vm: str) -> int:
-        if cloud_provider == "aws":
-            n_vcpus = re.findall(r"\d*x", vm)[0]
-            return 4 * int(n_vcpus.replace("x", "") or 1) if "x" in n_vcpus else int(n_vcpus)
-        elif cloud_provider == "azure":
-            n_vcpus = re.findall(r"\d+", vm)
-            return int(n_vcpus[0])
-        else:
-            return int(vm.split("-")[-1])
+    def _vm_to_vcpus(self, cloud_provider: str, vm: str) -> int:
+        """Gets the vcpu_cost of the given vm instance (instance_name)
 
-    @staticmethod
-    def _vcpus_to_vm(cloud_provider: str, vcpus: int) -> str:
-        if cloud_provider == "aws":
-            return "m5.large" if vcpus == 2 else "m5.xlarge" if vcpus == 4 else f"m5.{vcpus // 4}xlarge"
-        vm_family = "Standard_D{}_v5" if cloud_provider == "azure" else "n2-standard-{}"
-        return vm_family.format(vcpus)
-
-    # @staticmethod
-    # def _split_vcpus(num_vcpus: int, quota_limit: int) -> Optional[Tuple]:
-    #     """Splits the total/target number of vcpus used into smaller partitions according to the quota limit with the
-    #     given vcpu options. These options are the same for all providers for now. Enforces that the vcpu_count is
-    #     all the same for all partitions, meaning that for a num_vcpus=32, you cannot partition into 1 vm of 16 vcpus and
-    #     2 vms of 8 vcpus but can partition into 4 vms of 8 vcpus. Returns the number of partitions and their vcpus usage
-    #     """
-    #     # If the desired vCPU count is within the quota limit, don't fall back
-    #     if num_vcpus <= quota_limit:
-    #         return (1, num_vcpus)
-
-    #     # Otherwise, try to split the desired vCPU count into smaller portions that are within the quota limit and use the largest option
-    #     else:
-    #         for vcpu_count in Planner._VCPUS:
-    #             if vcpu_count <= quota_limit and vcpu_count <= num_vcpus:
-    #                 portions = num_vcpus // vcpu_count
-    #                 remaining_vcpus = num_vcpus - (vcpu_count * portions)
-    #                 # If the remaining vCPUs are 0, use the current option to launch all portions
-    #                 if remaining_vcpus == 0:
-    #                     return (portions, vcpu_count)  # [vcpu_count] * portions
-    #     # Return None if no valid vCPU portions were found
-    #     return None
+        :param cloud_provider: name of the cloud_provider
+        :type cloud_provider: str
+        :param instance_name: name of the vm instance
+        :type instance_name: str
+        """
+        return self.vcpu_info[cloud_provider][vm]
 
 
 class UnicastDirectPlanner(Planner):
@@ -174,13 +154,8 @@ class MulticastDirectPlanner(Planner):
 
         plan = TopologyPlan(src_region_tag=src_region_tag, dest_region_tags=dst_region_tags)
 
-        # Dynammically calculate n_instances based on quota limits - do_parallel returns
-        # tuples of (vcpus, n_instances)
-        vm_info = do_parallel(self._calculate_vm_types, [src_region_tag] + dst_region_tags)  # type: ignore
-        vm_types = {v[0]: Planner._vcpus_to_vm(cloud_provider=v[0].split(":")[0], vcpus=v[1][0]) for v in vm_info}  # type: ignore
-
-        # Taking the minimum so that we can use the same number of instances for both source and destination
-        n_instances = min(v[1][1] for v in vm_info)  # type: ignore
+        # Dynammically calculate n_instances based on quota limits
+        vm_types, n_instances = self._get_vm_type_and_instances(src_region_tag=src_region_tag, dst_region_tags=dst_region_tags)
 
         # TODO: support on-sided transfers but not requiring VMs to be created in source/destination regions
         for i in range(n_instances):
@@ -289,7 +264,7 @@ class MulticastDirectPlanner(Planner):
                     return quota["spot_standard_vcpus"] if spot else quota["on_demand_standard_vcpus"]
         return None
 
-    def _calculate_vm_types(self, region_tag: str) -> Optional[Tuple[int, int]]:
+    def _calculate_vm_types(self, region_tag: str) -> Optional[Tuple[str, int]]:
         """Calculates the largest allowed vm type according to the regional quota limit as well as
         how many of these vm types can we launch to avoid QUOTA_EXCEEDED errors. Returns None if quota
         information wasn't properly loaded or allowed vcpu list is wrong.
@@ -309,28 +284,46 @@ class MulticastDirectPlanner(Planner):
             return None
 
         config_vm_type = getattr(self.transfer_config, f"{cloud_provider}_instance_class")
-        config_vcpus = Planner._vm_to_vcpus(cloud_provider, config_vm_type)
+        config_vcpus = self._vm_to_vcpus(cloud_provider, config_vm_type)
         if config_vcpus <= quota_limit:
-            return config_vcpus, quota_limit // config_vcpus
+            return config_vm_type, quota_limit // config_vcpus
 
-        vcpus = None
-        for value in Planner._VCPUS:
-            if value <= quota_limit:
-                vcpus = value
+        vm_type, vcpus = None, None
+        for instance_name, vcpu_cost in sorted(self.vcpu_info[cloud_provider].items(), key=lambda x: x[1], reverse=True):
+            if vcpu_cost <= quota_limit:
+                vm_type, vcpus = instance_name, vcpu_cost
                 break
 
         # shouldn't happen, but just in case we use more complicated vm types in the future
-        if vcpus is None:
+        if vm_type is None or vcpus is None:
             return None
 
         # number of instances allowed by the quota with the selected vm type
         n_instances = quota_limit // vcpus
         logger.warning(
-            f"Falling back to instance class `{Planner._vcpus_to_vm(cloud_provider, vcpus)}` at {region_tag} "
+            f"Falling back to instance class `{vm_type}` at {region_tag} "
             f"due to cloud vCPU limit of {quota_limit}. You can visit https://skyplane.org/en/latest/increase_vcpus.html "
             "to learn more about how to increase your cloud vCPU limits for any cloud provider."
         )
-        return (vcpus, n_instances)
+        return (vm_type, n_instances)
+
+    def _get_vm_type_and_instances(self, src_region_tag: str, dst_region_tags: List[str]) -> Tuple[Dict[str, str], int]:
+        """Dynamically calculates the vm type each region can use (both the source region and all destination regions)
+        based on their quota limits and calculates the number of vms to launch in all regions by conservatively
+        taking the minimum of all regions to stay consistent.
+
+        :param src_region_tag: the source region tag
+        :type src_region_tag: str
+        :param dst_region_tags: a list of the destination region tags
+        :type dst_region_tags: List[str]
+        """
+        # do_parallel returns tuples of (region_tag, (vm_type, n_instances))
+        vm_info = do_parallel(self._calculate_vm_types, [src_region_tag] + dst_region_tags)
+        # Specifies the vm_type for each region
+        vm_types = {v[0]: v[1][0] for v in vm_info}  # type: ignore
+        # Taking the minimum so that we can use the same number of instances for both source and destination
+        n_instances = min(v[1][1] for v in vm_info)  # type: ignore
+        return vm_types, n_instances
 
 
 class DirectPlannerSourceOneSided(MulticastDirectPlanner):
--- tests/test_fall_back.py
+++ tests/test_fall_back.py
@@ -1,40 +1,89 @@
-# from typing import Dict, Tuple
-# from skyplane import compute
-# from skyplane.api.config import TransferConfig
-# from skyplane.planner.planner import MulticastDirectPlanner, Planner
-# from skyplane.utils.fn import do_parallel
-
-# quota_limits = {
-#     "aws": [
-#         {"on_demand_standard_vcpus": 5, "spot_standard_vcpus": 5, "region_name": "us-east-1"},
-#     ],
-#     "gcp": {
-#         "us-east1": 8,
-#     },
-#     "azure": {
-#         "uaenorth": 4,
-#     },
-# }
+from skyplane.api.config import TransferConfig
+from skyplane.cli.cli_transfer import SkyplaneCLI
+from skyplane.config import SkyplaneConfig
+from skyplane.planner.planner import MulticastDirectPlanner
 
 
-# def test_fall_back():
-#     transfer_config = TransferConfig()
-#     planner = MulticastDirectPlanner(n_instances=8, n_connections=100, transfer_config=transfer_config)
-#     planner.quota_limits = quota_limits
+def test_fall_back_multiple_limits_and_types():
+    # Regions for each provider
+    regions = {
+        "aws": "us-east-1",
+        "azure": "uaenorth",
+        "gcp": "us-east1",
+    }
 
-#     region_tags = ["aws:us-east-1", "azure:uaenorth", "gcp:us-east1"]
-#     test_vm_types = {"aws:us-east-1": "m5.xlarge", "azure:uaenorth": "Standard_D4_v5", "gcp:us-east1": "n2-standard-8"}
-#     test_n_instances = {
-#         "aws:us-east-1": 1,
-#         "azure:uaenorth": 1,
-#         "gcp:us-east1": 1,
-#     }
+    # Different quota limits
+    tests = [
+        [
+            {
+                "aws": [
+                    {
+                        "on_demand_standard_vcpus": 5,
+                        "spot_standard_vcpus": 5,
+                        "region_name": "us-east-1",
+                    }
+                ],
+                "gcp": {"us-east1": 8},
+                "azure": {"uaenorth": 4},
+            },
+            {
+                "azure:uaenorth": "Standard_D2_v5",
+                "aws:us-east-1": "m5.xlarge",
+                "gcp:us-east1": "n2-standard-8",
+            },
+            1,
+        ],
+        [
+            {
+                "aws": [
+                    {
+                        "on_demand_standard_vcpus": 10,
+                        "spot_standard_vcpus": 10,
+                        "region_name": "us-east-1",
+                    }
+                ],
+                "gcp": {"us-east1": 16},
+                "azure": {"uaenorth": 8},
+            },
+            {
+                "azure:uaenorth": "Standard_D2_v5",
+                "gcp:us-east1": "n2-standard-16",
+                "aws:us-east-1": "m5.2xlarge",
+            },
+            1,
+        ],
+        [
+            {
+                "aws": [
+                    {
+                        "on_demand_standard_vcpus": 20,
+                        "spot_standard_vcpus": 20,
+                        "region_name": "us-east-1",
+                    }
+                ],
+                "gcp": {"us-east1": 32},
+                "azure": {"uaenorth": 16},
+            },
+            {
+                "azure:uaenorth": "Standard_D2_v5",
+                "gcp:us-east1": "n2-standard-16",
+                "aws:us-east-1": "m5.4xlarge",
+            },
+            1,
+        ],
+    ]
 
-#     for i, src_region_tag in enumerate(region_tags):
-#         dst_region_tags = region_tags[:i] + region_tags[i + 1 :]
-#         vm_info = do_parallel(planner._calculate_vm_types, [src_region_tag] + dst_region_tags)  # type: ignore
-#         vm_types = {v[0]: Planner._vcpus_to_vm(cloud_provider=v[0].split(":")[0], vcpus=v[1][0]) for v in vm_info}  # type: ignore
-#         n_instances = min(v[1][1] for v in vm_info)  # type: ignore
+    for test in tests:
+        quota_limits, expected_vm_types, expected_n_instances = test
+        transfer_config = TransferConfig()
+        planner = MulticastDirectPlanner(n_instances=8, n_connections=100, transfer_config=transfer_config)
+        planner.quota_limits = quota_limits
 
-#         assert vm_types == test_vm_types, "vm types are calculated wrong"
-#         assert test_n_instances[src_region_tag] == n_instances
+        region_tags = [f"{p}:{regions[p]}" for p in regions.keys()]
+        for i, src_region_tag in enumerate(region_tags):
+            dst_region_tags = region_tags[:i] + region_tags[i + 1 :]
+            vm_types, n_instances = planner._get_vm_type_and_instances(src_region_tag=src_region_tag, dst_region_tags=dst_region_tags)
+
+            assert vm_types == expected_vm_types, f"vm types are calculated wrong {vm_types}"
+            assert n_instances == expected_n_instances, f"n_instances are calculated wrong {n_instances}"
+    print("Test succeeded!")
