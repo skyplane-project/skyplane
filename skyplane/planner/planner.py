c601c34c1716883fb64058069dc4dbbc81eee684
Lütfi Eren Erdoğan <lerdogan@berkeley.edu>
2023-06-07 20:49:20 -0700

    little changes to the fall back logic + tests (#859)
    Instead of storing the vcpu limits as a hardcoded variable inside
    Planner, created a new .csv file that includes that information. When
    Planner is created, we read this csv file.
    
    Wrote test cases that include fake quota limits.

--- skyplane/planner/planner.py
+++ skyplane/planner/planner.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 from importlib.resources import path
 from typing import Dict, List, Optional, Tuple, Tuple
 import re
+import os
+import csv
 
 from skyplane import compute
 from skyplane.api.config import TransferConfig
@@ -30,46 +33,112 @@ class Planner:
     def plan(self) -> TopologyPlan:
         raise NotImplementedError
 
-    @staticmethod
-    def _vm_to_vcpus(cloud_provider: str, vm: str) -> int:
-        if cloud_provider == "aws":
-            n_vcpus = re.findall(r"\d*x", vm)[0]
-            return 4 * int(n_vcpus.replace("x", "") or 1) if "x" in n_vcpus else int(n_vcpus)
+    def _vm_to_vcpus(self, cloud_provider: str, vm: str) -> int:
+        """Gets the vcpu_cost of the given vm instance (instance_name)
+
+        :param cloud_provider: name of the cloud_provider
+        :type cloud_provider: str
+        :param instance_name: name of the vm instance
+        :type instance_name: str
+        """
+        return self.vcpu_info[cloud_provider][vm]
+
+    def _get_quota_limits_for(self, cloud_provider: str, region: str, spot: bool = False) -> Optional[int]:
+        """Gets the quota info from the saved files. Returns None if quota_info isn't loaded during `skyplane init`
+        or if the quota info doesn't include the region.
+
+        :param cloud_provider: name of the cloud provider of the region
+        :type cloud_provider: str
+        :param region: name of the region for which to get the quota for
+        :type region: int
+        :param spot: whether to use spot specified by the user config (default: False)
+        :type spot: bool
+        """
+        quota_limits = self.quota_limits[cloud_provider]
+        if not quota_limits:
+            # User needs to reinitialize to save the quota information
+            logger.warning(f"Please run `skyplane init --reinit-{cloud_provider}` to load the quota information")
+            return None
+        if cloud_provider == "gcp":
+            region_family = "-".join(region.split("-")[:2])
+            if region_family in quota_limits:
+                return quota_limits[region_family]
         elif cloud_provider == "azure":
-            n_vcpus = re.findall(r"\d+", vm)
-            return int(n_vcpus[0])
-        else:
-            return int(vm.split("-")[-1])
+            if region in quota_limits:
+                return quota_limits[region]
+        elif cloud_provider == "aws":
+            for quota in quota_limits:
+                if quota["region_name"] == region:
+                    return quota["spot_standard_vcpus"] if spot else quota["on_demand_standard_vcpus"]
+        return None
 
-    @staticmethod
-    def _vcpus_to_vm(cloud_provider: str, vcpus: int) -> str:
-        if cloud_provider == "aws":
-            return "m5.large" if vcpus == 2 else "m5.xlarge" if vcpus == 4 else f"m5.{vcpus // 4}xlarge"
-        vm_family = "Standard_D{}_v5" if cloud_provider == "azure" else "n2-standard-{}"
-        return vm_family.format(vcpus)
+    def _calculate_vm_types(self, region_tag: str) -> Optional[Tuple[str, int]]:
+        """Calculates the largest allowed vm type according to the regional quota limit as well as
+        how many of these vm types can we launch to avoid QUOTA_EXCEEDED errors. Returns None if quota
+        information wasn't properly loaded or allowed vcpu list is wrong.
 
-    # @staticmethod
-    # def _split_vcpus(num_vcpus: int, quota_limit: int) -> Optional[Tuple]:
-    #     """Splits the total/target number of vcpus used into smaller partitions according to the quota limit with the
-    #     given vcpu options. These options are the same for all providers for now. Enforces that the vcpu_count is
-    #     all the same for all partitions, meaning that for a num_vcpus=32, you cannot partition into 1 vm of 16 vcpus and
-    #     2 vms of 8 vcpus but can partition into 4 vms of 8 vcpus. Returns the number of partitions and their vcpus usage
-    #     """
-    #     # If the desired vCPU count is within the quota limit, don't fall back
-    #     if num_vcpus <= quota_limit:
-    #         return (1, num_vcpus)
+        :param region_tag: tag of the node we are calculating the above for, example -> "aws:us-east-1"
+        :type region_tag: str
+        """
+        cloud_provider, region = region_tag.split(":")
 
-    #     # Otherwise, try to split the desired vCPU count into smaller portions that are within the quota limit and use the largest option
-    #     else:
-    #         for vcpu_count in Planner._VCPUS:
-    #             if vcpu_count <= quota_limit and vcpu_count <= num_vcpus:
-    #                 portions = num_vcpus // vcpu_count
-    #                 remaining_vcpus = num_vcpus - (vcpu_count * portions)
-    #                 # If the remaining vCPUs are 0, use the current option to launch all portions
-    #                 if remaining_vcpus == 0:
-    #                     return (portions, vcpu_count)  # [vcpu_count] * portions
-    #     # Return None if no valid vCPU portions were found
-    #     return None
+        # Get the quota limit
+        quota_limit = self._get_quota_limits_for(
+            cloud_provider=cloud_provider, region=region, spot=getattr(self.transfer_config, f"{cloud_provider}_use_spot_instances")
+        )
+
+        # No quota limits (quota limits weren't initialized properly during skyplane init)
+        if quota_limit is None:
+            return None
+
+        config_vm_type = getattr(self.transfer_config, f"{cloud_provider}_instance_class")
+        config_vcpus = self._vm_to_vcpus(cloud_provider, config_vm_type)
+        if config_vcpus <= quota_limit:
+            return config_vm_type, quota_limit // config_vcpus
+
+        vm_type, vcpus = None, None
+        for instance_name, vcpu_cost in sorted(self.vcpu_info[cloud_provider].items(), key=lambda x: x[1], reverse=True):
+            if vcpu_cost <= quota_limit:
+                vm_type, vcpus = instance_name, vcpu_cost
+                break
+
+        # shouldn't happen, but just in case we use more complicated vm types in the future
+        if vm_type is None or vcpus is None:
+            return None
+
+        # number of instances allowed by the quota with the selected vm type
+        n_instances = quota_limit // vcpus
+        logger.warning(
+            f"Falling back to instance class `{vm_type}` at {region_tag} "
+            f"due to cloud vCPU limit of {quota_limit}. You can visit https://skyplane.org/en/latest/increase_vcpus.html "
+            "to learn more about how to increase your cloud vCPU limits for any cloud provider."
+        )
+        return (vm_type, n_instances)
+
+    def _get_vm_type_and_instances(
+        self, src_region_tag: Optional[str] = None, dst_region_tags: Optional[List[str]] = None
+    ) -> Tuple[Dict[str, str], int]:
+        """Dynamically calculates the vm type each region can use (both the source region and all destination regions)
+        based on their quota limits and calculates the number of vms to launch in all regions by conservatively
+        taking the minimum of all regions to stay consistent.
+
+        :param src_region_tag: the source region tag (default: None)
+        :type src_region_tag: Optional[str]
+        :param dst_region_tags: a list of the destination region tags (defualt: None)
+        :type dst_region_tags: Optional[List[str]]
+        """
+        # One of them has to provided
+        assert src_region_tag is not None or dst_region_tags is not None, "There needs to be at least one source or destination"
+        src_tags = [src_region_tag] if src_region_tag is not None else []
+        dst_tags = dst_region_tags or []
+
+        # do_parallel returns tuples of (region_tag, (vm_type, n_instances))
+        vm_info = do_parallel(self._calculate_vm_types, src_tags + dst_tags)
+        # Specifies the vm_type for each region
+        vm_types = {v[0]: v[1][0] for v in vm_info}  # type: ignore
+        # Taking the minimum so that we can use the same number of instances for both source and destination
+        n_instances = min([self.n_instances] + [v[1][1] for v in vm_info])  # type: ignore
+        return vm_types, n_instances
 
 
 class UnicastDirectPlanner(Planner):
@@ -92,11 +161,14 @@ class UnicastDirectPlanner(Planner):
             assert job.dst_ifaces[0].region_tag() == dst_region_tag, "All jobs must have same destination region"
 
         plan = TopologyPlan(src_region_tag=src_region_tag, dest_region_tags=[dst_region_tag])
-        # TODO: use VM limits to determine how many instances to create in each region
+
+        # Dynammically calculate n_instances based on quota limits
+        vm_types, n_instances = self._get_vm_type_and_instances(src_region_tag=src_region_tag, dst_region_tags=[dst_region_tag])
+
         # TODO: support on-sided transfers but not requiring VMs to be created in source/destination regions
-        for i in range(self.n_instances):
-            plan.add_gateway(src_region_tag)
-            plan.add_gateway(dst_region_tag)
+        for i in range(n_instances):
+            plan.add_gateway(src_region_tag, vm_types[src_region_tag])
+            plan.add_gateway(dst_region_tag, vm_types[dst_region_tag])
 
         # ids of gateways in dst region
         dst_gateways = plan.get_region_gateways(dst_region_tag)
@@ -116,7 +188,7 @@ class UnicastDirectPlanner(Planner):
                 GatewayReadObjectStore(src_bucket, src_region_tag, self.n_connections), partition_id=partition_id
             )
             mux_or = src_program.add_operator(GatewayMuxOr(), parent_handle=obj_store_read, partition_id=partition_id)
-            for i in range(self.n_instances):
+            for i in range(n_instances):
                 src_program.add_operator(
                     GatewaySend(target_gateway_id=dst_gateways[i].gateway_id, region=src_region_tag, num_connections=self.n_connections),
                     parent_handle=mux_or,
@@ -170,14 +242,8 @@ class MulticastDirectPlanner(Planner):
 
         plan = TopologyPlan(src_region_tag=src_region_tag, dest_region_tags=dst_region_tags)
 
-        # Dynammically calculate n_instances based on quota limits - do_parallel returns
-        # tuples of (vcpus, n_instances)
-        vm_info = do_parallel(self._calculate_vm_types, [src_region_tag] + dst_region_tags)  # type: ignore
-        vm_types = {v[0]: Planner._vcpus_to_vm(cloud_provider=v[0].split(":")[0], vcpus=v[1][0]) for v in vm_info}  # type: ignore
-
-        # Taking the minimum so that we can use the same number of instances for both source and destination
-        n_instances = min(self.n_instances, min(v[1][1] for v in vm_info))  # type: ignore
-        print("n_instances", n_instances)
+        # Dynammically calculate n_instances based on quota limits
+        vm_types, n_instances = self._get_vm_type_and_instances(src_region_tag=src_region_tag, dst_region_tags=dst_region_tags)
 
         # TODO: support on-sided transfers but not requiring VMs to be created in source/destination regions
         for i in range(n_instances):
@@ -257,78 +323,6 @@ class MulticastDirectPlanner(Planner):
                 plan.set_gateway_program(dst_region_tag, program)
         return plan
 
-    def _get_quota_limits_for(self, cloud_provider: str, region: str, spot: bool = False) -> Optional[int]:
-        """Gets the quota info from the saved files. Returns None if quota_info isn't loaded during `skyplane init`
-        or if the quota info doesn't include the region.
-
-        :param cloud_provider: name of the cloud provider of the region
-        :type cloud_provider: str
-        :param region: name of the region for which to get the quota for
-        :type region: int
-        :param spot: whether to use spot specified by the user config (default: False)
-        :type spot: bool
-        """
-        quota_limits = self.quota_limits[cloud_provider]
-        if not quota_limits:
-            # User needs to reinitialize to save the quota information
-            logger.warning(f"Please run `skyplane init --reinit-{cloud_provider}` to load the quota information")
-            return None
-        if cloud_provider == "gcp":
-            region_family = "-".join(region.split("-")[:2])
-            if region_family in quota_limits:
-                return quota_limits[region_family]
-        elif cloud_provider == "azure":
-            if region in quota_limits:
-                return quota_limits[region]
-        elif cloud_provider == "aws":
-            for quota in quota_limits:
-                if quota["region_name"] == region:
-                    return quota["spot_standard_vcpus"] if spot else quota["on_demand_standard_vcpus"]
-        return None
-
-    def _calculate_vm_types(self, region_tag: str) -> Optional[Tuple[int, int]]:
-        """Calculates the largest allowed vm type according to the regional quota limit as well as
-        how many of these vm types can we launch to avoid QUOTA_EXCEEDED errors. Returns None if quota
-        information wasn't properly loaded or allowed vcpu list is wrong.
-
-        :param region_tag: tag of the node we are calculating the above for, example -> "aws:us-east-1"
-        :type region_tag: str
-        """
-        cloud_provider, region = region_tag.split(":")
-
-        # Get the quota limit
-        quota_limit = self._get_quota_limits_for(
-            cloud_provider=cloud_provider, region=region, spot=getattr(self.transfer_config, f"{cloud_provider}_use_spot_instances")
-        )
-
-        # No quota limits (quota limits weren't initialized properly during skyplane init)
-        if quota_limit is None:
-            return None
-
-        config_vm_type = getattr(self.transfer_config, f"{cloud_provider}_instance_class")
-        config_vcpus = Planner._vm_to_vcpus(cloud_provider, config_vm_type)
-        if config_vcpus <= quota_limit:
-            return config_vcpus, quota_limit // config_vcpus
-
-        vcpus = None
-        for value in Planner._VCPUS:
-            if value <= quota_limit:
-                vcpus = value
-                break
-
-        # shouldn't happen, but just in case we use more complicated vm types in the future
-        if vcpus is None:
-            return None
-
-        # number of instances allowed by the quota with the selected vm type
-        n_instances = quota_limit // vcpus
-        logger.warning(
-            f"Falling back to instance class `{Planner._vcpus_to_vm(cloud_provider, vcpus)}` at {region_tag} "
-            f"due to cloud vCPU limit of {quota_limit}. You can visit https://skyplane.org/en/latest/increase_vcpus.html "
-            "to learn more about how to increase your cloud vCPU limits for any cloud provider."
-        )
-        return (vcpus, n_instances)
-
 
 class DirectPlannerSourceOneSided(Planner):
     """Planner that only creates VMs in the source region"""
@@ -347,10 +341,13 @@ class DirectPlannerSourceOneSided(Planner):
             assert [iface.region_tag() for iface in job.dst_ifaces] == dst_region_tags, "Add jobs must have same destination set"
 
         plan = TopologyPlan(src_region_tag=src_region_tag, dest_region_tags=dst_region_tags)
-        # TODO: use VM limits to determine how many instances to create in each region
+
+        # Dynammically calculate n_instances based on quota limits
+        vm_types, n_instances = self._get_vm_type_and_instances(src_region_tag=src_region_tag)
+
         # TODO: support on-sided transfers but not requiring VMs to be created in source/destination regions
-        for i in range(self.n_instances):
-            plan.add_gateway(src_region_tag)
+        for i in range(n_instances):
+            plan.add_gateway(src_region_tag, vm_types[src_region_tag])
 
         # initialize gateway programs per region
         src_program = GatewayProgram()
@@ -410,11 +407,14 @@ class DirectPlannerDestOneSided(Planner):
             assert [iface.region_tag() for iface in job.dst_ifaces] == dst_region_tags, "Add jobs must have same destination set"
 
         plan = TopologyPlan(src_region_tag=src_region_tag, dest_region_tags=dst_region_tags)
-        # TODO: use VM limits to determine how many instances to create in each region
+
+        # Dynammically calculate n_instances based on quota limits
+        vm_types, n_instances = self._get_vm_type_and_instances(dst_region_tags=dst_region_tags)
+
         # TODO: support on-sided transfers but not requiring VMs to be created in source/destination regions
-        for i in range(self.n_instances):
+        for i in range(n_instances):
             for dst_region_tag in dst_region_tags:
-                plan.add_gateway(dst_region_tag)
+                plan.add_gateway(dst_region_tag, vm_types[dst_region_tag])
 
         # initialize gateway programs per region
         dst_program = {dst_region: GatewayProgram() for dst_region in dst_region_tags}
