1c185b4716296c82a1cb5b6380b2df33a86552b9
Sarah Wooders <sarahwooders@gmail.com>
2023-06-12 12:58:37 -0700

    Disable cloudflare for integration tests (#864)

--- skyplane/cli/cli_init.py
+++ skyplane/cli/cli_init.py
@@ -535,9 +535,9 @@ def init(
             cloud_config = load_gcp_config(cloud_config, force_init=reinit_gcp, non_interactive=non_interactive)
 
     # load cloudflare config
-    if not reinit_cloudflare:
-        typer.secho("\n(1) configuring cloudflare R2:", fg="yellow", bold=True)
-        if not disable_config_aws:
+    if not reinit_cloudflare:  # TODO: fix reinit logic
+        typer.secho("\n(4) Configuring Cloudflare R2:", fg="yellow", bold=True)
+        if not disable_config_cloudflare:
             cloud_config = load_cloudflare_config(cloud_config, non_interactive=non_interactive)
 
     # load IBMCloud config
